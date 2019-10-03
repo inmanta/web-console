02f282487dfa62a6ff6178f689ae42d2393ff147
Andras Kovari <andras.kovari@inmanta.com>
2019-10-03 12:43:30 +0200

    Different style for async function

--- src/app/ServiceCatalog/ServiceCatalog.tsx
+++ src/app/ServiceCatalog/ServiceCatalog.tsx
@@ -1,31 +1,37 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 import { CatalogAccordion } from './CatalogAccordion';
-import { useStoreState, State, useStoreDispatch } from 'easy-peasy';
+import { useStoreState, State, useStoreDispatch, Dispatch, Action } from 'easy-peasy';
 import { IStoreModel } from '@app/Models/core-models';
 
 const ServiceCatalog: React.FunctionComponent<any> = (props) => {
-  const project = useStoreState((store:State<IStoreModel>) => store.projects.selectedProject);
+  const project = useStoreState((store: State<IStoreModel>) => store.projects.selectedProject);
   const dispatch = useStoreDispatch<IStoreModel>();
-  
-  if (project.selectedEnvironment.id && !project.selectedEnvironment.services) {
 
-    fetch(process.env.API_BASEURL + "/lsm/v1/service_catalog", {
-      headers: {
-        "X-Inmanta-Tid": project.selectedEnvironment.id
-      }
-    }).then(result => result.json()).then(result => {
-      // console.log(result.data);
-      dispatch.projects.selectedProject.selectedEnvironment.addServices(result.data);
-    });
+  if (project.selectedEnvironment.id && !project.selectedEnvironment.services) {
+    fetchServiceCatalog(dispatch, project.selectedEnvironment.id);
   }
-  
+
   return (
     <PageSection>
       <Title size="lg">Service Catalog Page Title</Title>
       <CatalogAccordion services={project.selectedEnvironment.services} />
     </PageSection>
   );
+}
+
+async function fetchServiceCatalog(dispatch: Dispatch<IStoreModel, Action<any>>, environmentId: string) {
+  try {
+    const result = await fetch(process.env.API_BASEURL + "/lsm/v1/service_catalog", {
+      headers: {
+        "X-Inmanta-Tid": environmentId
+      }
+    })
+    const json = await result.json();
+    dispatch.projects.selectedProject.selectedEnvironment.addServices(json.data);
+  } catch (error) {
+    throw error;
+  }
 }
 
 export { ServiceCatalog };
