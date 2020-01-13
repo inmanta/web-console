7d2d93df9135c9eb7ae935931e585d8b51abcd52
Andras Kovari <andras.kovari@inmanta.com>
2020-01-13 10:17:16 +0100

    Add delete service entity functionality

--- src/app/ServiceCatalog/CatalogDataList.test.tsx
+++ src/app/ServiceCatalog/CatalogDataList.test.tsx
@@ -18,6 +18,8 @@ describe('Catalog Data List', () => {
     lifecycle: { initialState: '', states: [], transfers: [] },
     name: 'otherService',
   }];
+  const serviceCatalogUrl = '/lsm/v1/service_catalog';
+  const environmentId = 'env';
 
   it.each`
   services | numberOfItems
@@ -26,13 +28,13 @@ describe('Catalog Data List', () => {
   ${singleService} | ${1}
   ${doubleService} | ${2}
   `('Given services $services, should render $numberOfItems items', ({ services, numberOfItems }) => {
-    const wrapper = shallow(<CatalogDataList services={services} />);
+    const wrapper = shallow(<CatalogDataList services={services} environmentId={environmentId} serviceCatalogUrl={serviceCatalogUrl} />);
     const listItems = wrapper.find(DataListItem);
     expect(listItems).toHaveLength(numberOfItems);
   });
 
   it('Should render correct links for each service', () => {
-    const wrapper = shallow(<CatalogDataList services={doubleService} />);
+    const wrapper = shallow(<CatalogDataList services={doubleService} environmentId={environmentId} serviceCatalogUrl={serviceCatalogUrl} />);
     const listItems = wrapper.find(DataListItem);
     const buttonOfFirstService = listItems.first().find(DataListAction).children().first();
     const buttonOfLastService = listItems.last().find(DataListAction).children().first();
@@ -46,7 +48,7 @@ describe('Catalog Data List', () => {
   ${['button#otherService-toggle']} | ${['section#otherService-expand']} | ${['section#service1-expand']}
   ${['button#service1-toggle', 'button#otherService-toggle']} | ${['section#service1-expand', 'section#otherService-expand']} | ${[]}
   `('When clicking on toggles $toggles, should show $visibleItems and hide $hiddenItems', ({ toggles, visibleItems, hiddenItems }) => {
-    const wrapper = mount(<MemoryRouter><CatalogDataList services={doubleService} /></MemoryRouter>);
+    const wrapper = mount(<MemoryRouter><CatalogDataList services={doubleService} environmentId={environmentId} serviceCatalogUrl={serviceCatalogUrl} /></MemoryRouter>);
     toggles.map(toggle => {
       wrapper.find(toggle).simulate('click');
     });
@@ -58,7 +60,7 @@ describe('Catalog Data List', () => {
     });
   });
   it('Should hide expandable, when clicked on toggle twice', () => {
-    const wrapper = mount(<MemoryRouter><CatalogDataList services={doubleService} /></MemoryRouter>);
+    const wrapper = mount(<MemoryRouter><CatalogDataList services={doubleService} environmentId={environmentId} serviceCatalogUrl={serviceCatalogUrl} /></MemoryRouter>);
     wrapper.find('button#service1-toggle').simulate('click');
     wrapper.find('button#service1-toggle').simulate('click');
     expect(wrapper.find('section#service1-expand').props().hidden).toBeTruthy();
