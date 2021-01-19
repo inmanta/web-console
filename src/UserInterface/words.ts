2360fb803bc42f603b6393d0966d4dc243fee478
Stijn Coolen <stijn.coolen@inmanta.com>
2021-01-19 11:35:21 +0100

    Add description to words module

--- src/UserInterface/words.ts
+++ src/UserInterface/words.ts
@@ -1,3 +1,15 @@
+/**
+ * This "words" module is responsible for all the text that
+ * is presented to the user. This gives a nice overview.
+ * If we need to change something, we can do it here.
+ * We don't have to find the specific view. Values can also
+ * be reused if their meaning is the same.
+ *
+ * The key should be a meaningful combination of words
+ * seperated by dots. Using this method, you can have a
+ * value for "inventory", but also for "inventory.state".
+ */
+
 const dict = {
   "inventory.column.id": "Id",
   "inventory.column.state": "State",
