78f2970924cfa800adae9a9873d02e13a37a8e86
Stijn Coolen <stijn.coolen@inmanta.com>
2021-01-21 20:00:22 +0100

    Add TreeTable

---
+++ src/UI/Components/TreeTable/TreeExpansionManager.test.ts
@@ -0,0 +1,14 @@
+import { TreeExpansionManager } from "./TreeExpansionManager";
+
+test("TreeExpansionManager closes all children second toggle", () => {
+  const treeExpansionManager = new TreeExpansionManager(".");
+  const keys = ["a", "a.d", "a.e", "a.e.f", "b", "c"];
+  const state1 = treeExpansionManager.create(keys);
+
+  const state2 = treeExpansionManager.toggle(state1, "a");
+  const state3 = treeExpansionManager.toggle(state2, "a.e");
+  const state4 = treeExpansionManager.toggle(state3, "a.e.f");
+  const state5 = treeExpansionManager.toggle(state4, "a");
+
+  expect(state5).toEqual(state1);
+});
---
+++ src/UI/Components/TreeTable/TreeRow/TreeRowView.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Tr, Td } from "@patternfly/react-table";
+import styled from "styled-components";
+import { TreeRow } from "./TreeRow";
+import { Toggle } from "@/UI/Components";
+
+interface IndentProps {
+  level: number;
+  noToggle?: boolean;
+}
+
+const Indent: React.FC<IndentProps> = ({ level, children, noToggle }) => {
+  const space = level * 16 + (noToggle ? 48 : 0);
+  return <span style={{ marginLeft: `${space}px` }}>{children}</span>;
+};
+
+const CustomTd = styled(Td)`
+  --pf-c-table--cell--PaddingTop: 24px;
+  --pf-c-table--cell--PaddingBottom: 24px;
+`;
+
+interface RowProps {
+  row: TreeRow;
+}
+
+export const TreeRowView: React.FC<RowProps> = ({ row }) => {
+  switch (row.kind) {
+    case "Flat":
+      return (
+        <Tr>
+          <Td dataLabel={row.cell.label}>
+            <Indent level={0} noToggle>
+              {row.cell.value}
+            </Indent>
+          </Td>
+          {row.cells.map(({ label, value }) => (
+            <Td key={label} dataLabel={label}>
+              {value}
+            </Td>
+          ))}
+        </Tr>
+      );
+    case "Root":
+      return (
+        <Tr>
+          <Td dataLabel="name" colSpan={4}>
+            <Indent level={0}>
+              <Toggle expanded={row.isChildExpanded} onToggle={row.onToggle} />a
+            </Indent>
+          </Td>
+        </Tr>
+      );
+
+    case "Branch":
+      return (
+        <Tr isExpanded={row.isExpandedByParent}>
+          <CustomTd colSpan={4} dataLabel={row.cell.label}>
+            <Indent level={row.level}>
+              <Toggle expanded={row.isChildExpanded} onToggle={row.onToggle} />
+              {row.cell.value}
+            </Indent>
+          </CustomTd>
+        </Tr>
+      );
+
+    case "Leaf":
+      return (
+        <Tr isExpanded={row.isExpandedByParent}>
+          <CustomTd dataLabel={row.cell.label}>
+            <Indent level={row.level} noToggle>
+              {row.cell.value}
+            </Indent>
+          </CustomTd>
+          {row.cells.map(({ label, value }) => (
+            <Td key={label} dataLabel={label}>
+              {value}
+            </Td>
+          ))}
+        </Tr>
+      );
+  }
+};
