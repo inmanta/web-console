8f55aef39b76ac9eec20bf917b9a3eecc2f195b2
Stijn Coolen <stijn.coolen@inmanta.com>
2021-01-26 16:07:17 +0100

    Add eslint testing plugins & Fix InventoryTable test

--- src/UI/Inventory/InventoryTable.test.tsx
+++ src/UI/Inventory/InventoryTable.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { InventoryTable } from "./InventoryTable";
 import { rows, tablePresenter } from "@/Test";
 
@@ -10,8 +10,7 @@ test("InventoryTable can be expanded", async () => {
 
   // Act
   fireEvent.click(screen.getAllByRole("button")[0]);
-  await waitFor(() => screen.getByTestId(testid));
 
   // Assert
-  expect(screen.getByTestId(testid)).toBeVisible();
+  expect(await screen.findByTestId(testid)).toBeVisible();
 });
