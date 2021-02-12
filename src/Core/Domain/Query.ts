4f80dca1e35910e7ae0ed60b8687115c1be9eff3
Stijn Coolen <stijn.coolen@inmanta.com>
2021-02-12 10:24:19 +0100

    Cleanup Query

--- src/Core/Domain/Query.ts
+++ src/Core/Domain/Query.ts
@@ -12,14 +12,8 @@ export interface ResourcesQuery {
   qualifier: ResourcesQualifier;
 }
 
-export interface ServiceInstancesQuery {
-  kind: "ServiceInstances";
-  qualifier: { id: string };
-}
-
 export type Query = ResourcesQuery;
 
 export interface QueryResult {
   Resources: RemoteData.Type<string, ResourceModel[]>;
-  ServiceInstances: RemoteData.Type<string, string>;
 }
