32a0902044a140e9694e7373b75e7565456cd808
Stijn Coolen <stijn.coolen@inmanta.com>
2021-02-26 17:24:27 +0100

    Add test for delete in ServiceCatalog

--- src/app/ServiceCatalog/ServiceCatalog.test.tsx
+++ src/app/ServiceCatalog/ServiceCatalog.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { StoreProvider } from "easy-peasy";
 import { StaticSubscriptionController, DeferredFetcher, Service } from "@/Test";
 import { Either } from "@/Core";
@@ -95,6 +95,41 @@ test("ServiceCatalog shows updated empty", async () => {
   servicesSubscriptionController.executeAll();
 
   servicesFetcher.resolve(Either.right([]));
+
+  expect(
+    await screen.findByRole("region", { name: "ServiceCatalog-Empty" })
+  ).toBeInTheDocument();
+});
+
+test("ServiceCatalog removes service after deletion", async () => {
+  const { component, servicesFetcher } = setup();
+  render(component);
+
+  expect(
+    await screen.findByRole("region", { name: "ServiceCatalog-Loading" })
+  ).toBeInTheDocument();
+
+  servicesFetcher.resolve(Either.right([Service.A]));
+
+  expect(
+    await screen.findByRole("region", { name: "ServiceCatalog-Success" })
+  ).toBeInTheDocument();
+
+  fireEvent.click(screen.getByRole("button", { name: "Delete" }));
+
+  fetchMock.mockResponse(JSON.stringify({}));
+  fireEvent.click(screen.getByRole("button", { name: "Yes" }));
+
+  /**
+   * I believe we need to use this hack because fetch is still being used.
+   * Ideally we don't mock fetch, but we use a test class on which we can
+   * await requests. I'm not sure if jest-fetch-mock offers a way to wait
+   * for the mocks to finish. But if we had a way to wait for the requests
+   * to finish, we wouldn't need this hack.
+   */
+  setTimeout(() => {
+    servicesFetcher.resolve(Either.right([]));
+  }, 0);
 
   expect(
     await screen.findByRole("region", { name: "ServiceCatalog-Empty" })
