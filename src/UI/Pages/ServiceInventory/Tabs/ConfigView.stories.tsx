3250d6567420768c4d4cd5ce78dbf9dd29b424c6
Stijn Coolen <stijn.coolen@inmanta.com>
2021-03-26 09:43:11 +0100

    Switch order of toggles in story for ConfigView

--- src/UI/Pages/ServiceInventory/Tabs/ConfigView.stories.tsx
+++ src/UI/Pages/ServiceInventory/Tabs/ConfigView.stories.tsx
@@ -7,9 +7,9 @@ export default {
 };
 
 const settings = [
+  { name: "auto_update_designed", value: false, defaultValue: true },
   { name: "auto_designed", value: false, defaultValue: false },
   { name: "auto_creating", value: true, defaultValue: false },
-  { name: "auto_update_designed", value: false, defaultValue: true },
   { name: "auto_update_inprogress", value: true, defaultValue: true },
 ];
 
