b12f65353e670e043b513aa30add303a58a9f735
Stijn Coolen <stijn.coolen@inmanta.com>
2021-03-31 11:19:47 +0200

    Add filters & sort properties to ServiceInstancesQuery

---
+++ src/UI/Data/ServiceInstances/getServiceInstancesUrl.test.ts
@@ -0,0 +1,68 @@
+import { Query } from "@/Core";
+import { getServiceInstancesUrl } from "./getServiceInstancesUrl";
+
+test("getServiceInstancesUrl returns correct url for no filter & no sort", () => {
+  const name = "service_a";
+  const qualifier: Query.Qualifier<"ServiceInstances"> = {
+    name,
+    environment: "env_a",
+    filter: undefined,
+    sort: undefined,
+  };
+
+  expect(getServiceInstancesUrl(qualifier)).toMatch(
+    `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20`
+  );
+});
+
+test("getServiceInstancesUrl returns correct url for filter & no sort", () => {
+  const name = "service_a";
+  const qualifier: Query.Qualifier<"ServiceInstances"> = {
+    name,
+    environment: "env_a",
+    filter: {
+      state: ["up", "creating"],
+    },
+    sort: undefined,
+  };
+
+  expect(getServiceInstancesUrl(qualifier)).toMatch(
+    `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20&filter.state=up&filter.state=creating`
+  );
+});
+
+test("getServiceInstancesUrl returns correct url for sort & no filter", () => {
+  const name = "service_a";
+  const qualifier: Query.Qualifier<"ServiceInstances"> = {
+    name,
+    environment: "env_a",
+    filter: undefined,
+    sort: {
+      name: "state",
+      order: "ASC",
+    },
+  };
+
+  expect(getServiceInstancesUrl(qualifier)).toMatch(
+    `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20&sort=state.ASC`
+  );
+});
+
+test("getServiceInstancesUrl returns correct url for sort & filter", () => {
+  const name = "service_a";
+  const qualifier: Query.Qualifier<"ServiceInstances"> = {
+    name,
+    environment: "env_a",
+    filter: {
+      state: ["up", "creating"],
+    },
+    sort: {
+      name: "state",
+      order: "ASC",
+    },
+  };
+
+  expect(getServiceInstancesUrl(qualifier)).toMatch(
+    `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20&filter.state=up&filter.state=creating&sort=state.ASC`
+  );
+});
