13507eeaaeab30a7a9c39ea6c57837d8da920431
Stijn Coolen <stijn.coolen@inmanta.com>
2021-04-02 16:09:43 +0200

    Expand test for getServiceInstancesUrl

--- src/UI/Data/ServiceInstances/getServiceInstancesUrl.test.ts
+++ src/UI/Data/ServiceInstances/getServiceInstancesUrl.test.ts
@@ -66,3 +66,21 @@ test("getServiceInstancesUrl returns correct url for sort & filter", () => {
     `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20&filter.state=up&filter.state=creating&sort=state.asc`
   );
 });
+
+test("getServiceInstancesUrl returns correct url for empty filter", () => {
+  const name = "service_a";
+  const qualifier: Query.Qualifier<"ServiceInstances"> = {
+    name,
+    environment: "env_a",
+    filter: {
+      state: [],
+      id: [],
+      attribute_set_empty: [],
+      attribute_set_not_empty: [],
+    },
+  };
+
+  expect(getServiceInstancesUrl(qualifier)).toMatch(
+    `/lsm/v1/service_inventory/${name}?include_deployment_progress=True&limit=20`
+  );
+});
