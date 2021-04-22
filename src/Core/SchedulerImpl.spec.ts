2104714de26e927dbbf283d3b2dc6f7bf6304b6a
Stijn Coolen <stijn.coolen@inmanta.com>
2021-04-22 11:46:28 +0200

    Extend test for SchedulerImpl

--- src/Core/SchedulerImpl.spec.ts
+++ src/Core/SchedulerImpl.spec.ts
@@ -5,18 +5,10 @@ jest.useFakeTimers();
 
 // https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
 
-test("Scheduler", async () => {
+test("GIVEN a Scheduler WHEN registering a task after a cycle THEN executes all registered tasks", async () => {
   const scheduler = new SchedulerImpl(5000);
-
-  const taskA = {
-    effect: jest.fn(async () => undefined),
-    update: jest.fn(),
-  };
-
-  const taskB = {
-    effect: jest.fn(async () => undefined),
-    update: jest.fn(),
-  };
+  const taskA = { effect: jest.fn(async () => undefined), update: jest.fn() };
+  const taskB = { effect: jest.fn(async () => undefined), update: jest.fn() };
 
   scheduler.register("taskA", taskA);
 
@@ -27,7 +19,6 @@ test("Scheduler", async () => {
   expect(taskA.effect).not.toBeCalled();
 
   jest.advanceTimersByTime(4000);
-
   await flushPromises();
 
   expect(taskA.effect).toHaveBeenCalledTimes(1);
@@ -38,11 +29,90 @@ test("Scheduler", async () => {
   expect(taskB.effect).not.toBeCalled();
 
   jest.advanceTimersByTime(5000);
-
   await flushPromises();
 
   expect(taskA.effect).toHaveBeenCalledTimes(2);
   expect(taskA.update).toHaveBeenCalledTimes(2);
   expect(taskB.effect).toHaveBeenCalledTimes(1);
   expect(taskB.update).toHaveBeenCalledTimes(1);
+});
+
+test("Given a Scheduler WHEN unregistering a task before the first cycle THEN the task is not executed", async () => {
+  const scheduler = new SchedulerImpl(5000);
+  const taskA = { effect: jest.fn(async () => undefined), update: jest.fn() };
+
+  scheduler.register("taskA", taskA);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+
+  jest.advanceTimersByTime(4999);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+
+  scheduler.unregister("taskA");
+  jest.advanceTimersByTime(1);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+
+  jest.advanceTimersByTime(5000);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+});
+
+test("GIVEN a Scheduler WHEN unregistering a task during a cycle THEN the task is not executed at the end of that cycle", async () => {
+  const scheduler = new SchedulerImpl(5000);
+  const taskA = { effect: jest.fn(async () => undefined), update: jest.fn() };
+
+  scheduler.register("taskA", taskA);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+
+  jest.advanceTimersByTime(5000);
+  await flushPromises();
+
+  expect(taskA.effect).toHaveBeenCalledTimes(1);
+  expect(taskA.update).toHaveBeenCalledTimes(1);
+
+  scheduler.unregister("taskA");
+  jest.advanceTimersByTime(5000);
+  await flushPromises();
+
+  expect(taskA.effect).toHaveBeenCalledTimes(1);
+  expect(taskA.update).toHaveBeenCalledTimes(1);
+});
+
+test("GIVEN a Scheduler WHEN unregistering the first task THEN the first task is not executed but the remaining tasks are executed", async () => {
+  const scheduler = new SchedulerImpl(5000);
+  const taskA = { effect: jest.fn(async () => undefined), update: jest.fn() };
+  const taskB = { effect: jest.fn(async () => undefined), update: jest.fn() };
+
+  scheduler.register("taskA", taskA);
+  scheduler.register("taskB", taskB);
+
+  expect(taskA.effect).not.toBeCalled();
+  expect(taskA.update).not.toBeCalled();
+  expect(taskB.effect).not.toBeCalled();
+  expect(taskB.update).not.toBeCalled();
+
+  jest.advanceTimersByTime(5000);
+  await flushPromises();
+
+  expect(taskA.effect).toHaveBeenCalledTimes(1);
+  expect(taskA.update).toHaveBeenCalledTimes(1);
+  expect(taskB.effect).toHaveBeenCalledTimes(1);
+  expect(taskB.update).toHaveBeenCalledTimes(1);
+
+  scheduler.unregister("taskA");
+  jest.advanceTimersByTime(5000);
+  await flushPromises();
+
+  expect(taskA.effect).toHaveBeenCalledTimes(1);
+  expect(taskA.update).toHaveBeenCalledTimes(1);
+  expect(taskB.effect).toHaveBeenCalledTimes(2);
+  expect(taskB.update).toHaveBeenCalledTimes(2);
 });
