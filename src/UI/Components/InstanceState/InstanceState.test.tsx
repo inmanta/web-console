d3ad3fcf5ec90a9d6986c242bebcfbaa26b3e61f
Stijn Coolen <stijn.coolen@inmanta.com>
2021-04-29 09:52:29 +0200

    Improve InstanceState test

--- src/UI/Components/InstanceState/InstanceState.test.tsx
+++ src/UI/Components/InstanceState/InstanceState.test.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import { InstanceState } from "./InstanceState";
 import { render, screen } from "@testing-library/react";
 
-test("GIVEN State label WHEN (name,label) is ('up', 'success') THEN name is visible", async () => {
+test("GIVEN State label WHEN (name,label) is ('up','success') THEN name is visible", async () => {
   render(<InstanceState name="up" label="success" />);
-  const label = screen.getByRole("generic", { name: "InstanceState-up" });
-  expect(label).toBeVisible();
+  expect(screen.getByText("up")).toBeVisible();
 });
 
-test("GIVEN State label WHEN (name,label) is ('rejected', 'warning') THEN name is visible", async () => {
+test("GIVEN State label WHEN (name,label) is ('rejected','warning') THEN name is visible", async () => {
   render(<InstanceState name="rejected" label="warning" />);
-  const label = screen.getByRole("generic", { name: "InstanceState-rejected" });
-  expect(label).toBeVisible();
+  expect(screen.getByText("rejected")).toBeVisible();
 });
 
-test("GIVEN State label WHEN (name,label) is ('ordered', null) THEN name is visible", async () => {
+test("GIVEN State label WHEN (name,label) is ('ordered',undefined) THEN name is visible", async () => {
   render(<InstanceState name="ordered" />);
-  const label = screen.getByRole("generic", { name: "InstanceState-ordered" });
-  expect(label).toBeVisible();
+  expect(screen.getByText("ordered")).toBeVisible();
 });
