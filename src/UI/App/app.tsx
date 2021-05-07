69b3a2f0e6fdbeca6aa1059873814f3d5c73b8d2
Stijn Coolen <stijn.coolen@inmanta.com>
2021-05-07 15:27:11 +0200

    Remove AppRoutes

--- src/UI/App/app.tsx
+++ src/UI/App/app.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import "@patternfly/react-core/dist/styles/base.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppLayout } from "@/UI/App/AppLayout/AppLayout";
-import { AppRoutes } from "@/UI/App/routes";
 import "@/UI/App/app.css";
 import { KeycloakInitOptions } from "keycloak-js";
 import { KeycloakProvider } from "react-keycloak";
@@ -13,6 +12,7 @@ import {
   Bullseye,
 } from "@patternfly/react-core";
 import { EnvironmentProvider } from "@/UI/Components";
+import { PageRouterWithDependencies } from "@/UI/Routing";
 
 const keycloakInitConfig = {
   onLoad: "login-required",
@@ -47,7 +47,7 @@ export const App: React.FunctionComponent<{
         <EnvironmentProvider
           Wrapper={({ children }) => <>{children}</>}
           Dependant={({ environment }) => (
-            <AppRoutes environment={environment} />
+            <PageRouterWithDependencies environment={environment} />
           )}
         />
       </AppLayout>
--- src/UI/Routing/PageRouter.tsx
+++ src/UI/Routing/PageRouter.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { NotFound } from "@/UI/Pages";
 import { useDocumentTitle } from "./useDocumentTitle";
 import { pages } from "./Page";
 import { getUrl } from "./Utils";
 
-export const PageRouter: React.FC = () => (
+import { DependencyManagerContext, DependencyProvider } from "@/UI/Dependency";
+
+export const PageRouterWithDependencies: React.FC<{ environment: string }> = ({
+  environment,
+}) => {
+  const dependencyManager = useContext(DependencyManagerContext);
+  const dependencies = dependencyManager.getDependencies(environment);
+
+  return (
+    <DependencyProvider dependencies={dependencies}>
+      <PageRouter />
+    </DependencyProvider>
+  );
+};
+
+const PageRouter: React.FC = () => (
   <Switch>
     {pages.map(({ path, label, kind, component }) => (
       <Route path={path} exact component={component} key={kind} title={label} />
