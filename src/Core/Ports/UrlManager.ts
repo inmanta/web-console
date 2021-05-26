4aa6a193ac206e2e6ad990d84e6a58286c0959e5
Stijn Coolen <stijn.coolen@inmanta.com>
2021-05-26 12:46:41 +0200

    Fix missing urlManager

--- src/Core/Ports/UrlManager.ts
+++ src/Core/Ports/UrlManager.ts
@@ -1,4 +1,5 @@
 export interface UrlManager {
+  setEnvironment(environment: string): void;
   getCompileReportUrl(): string;
   getModelVersionUrl(version: string): string;
   getResourceUrl(resourceId: string): string;
--- src/UI/Dependency/DummyUrlManager.ts
+++ src/UI/Dependency/DummyUrlManager.ts
@@ -1,6 +1,9 @@
 import { UrlManager } from "@/Core";
 
 export class DummyUrlManager implements UrlManager {
+  setEnvironment(): void {
+    throw new Error("Method not implemented.");
+  }
   getResourceUrl(): string {
     throw new Error("Method not implemented.");
   }
