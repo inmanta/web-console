d3838c89bc9cf7b9cee45b404c000aa8caf3ff89
Andras Kovari <andras.kovari@inmanta.com>
2021-05-28 10:47:38 +0200

    Update jest-fetch-mock config

--- jestSetup.ts
+++ jestSetup.ts
@@ -1,7 +1,3 @@
-import { GlobalWithFetchMock } from "jest-fetch-mock";
+import { enableFetchMocks } from "jest-fetch-mock";
 
-const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
-customGlobal.fetch = require("jest-fetch-mock");
-customGlobal.fetchMock = customGlobal.fetch;
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-(window as any).globalThis = window;
+enableFetchMocks();
