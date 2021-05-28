b2869fac62755cfaaf88356dd1f69d99d38cd2a5
Stijn Coolen <stijn.coolen@inmanta.com>
2021-05-28 11:11:38 +0200

    Tweak Resource test data

--- src/Test/Data/Resource.ts
+++ src/Test/Data/Resource.ts
@@ -2,22 +2,22 @@ import { ResourceModel } from "@/Core";
 
 export const a: ResourceModel = {
   resource_id: "resource_id_a",
-  resource_state: "resource_state_a",
+  resource_state: "deployed",
 };
 
 export const b: ResourceModel = {
   resource_id: "resource_id_b",
-  resource_state: "resource_state_b",
+  resource_state: "failed",
 };
 
 export const c: ResourceModel = {
   resource_id: "resource_id_c",
-  resource_state: "resource_state_c",
+  resource_state: "skipped",
 };
 
 export const d: ResourceModel = {
   resource_id: "resource_id_d",
-  resource_state: "resource_state_d",
+  resource_state: "deploying",
 };
 
 export const listA: ResourceModel[] = [a, b];
