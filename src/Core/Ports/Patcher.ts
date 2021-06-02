e301087ea4259c1d885bcf8a3c59d0e90740e0e6
Andras Kovari <andras.kovari@inmanta.com>
2021-06-02 10:25:38 +0200

    Docstring

--- src/Core/Ports/Patcher.ts
+++ src/Core/Ports/Patcher.ts
@@ -4,9 +4,8 @@ import { Command } from "@/Core/Domain";
 /**
  * The Patcher is responsible for sending patch requests to the API and receiving a response.
  *
- * @param {string} environment The environment for the entity in question
- * @param {string} url The endpoint
- * @param {object} body The POST body that needs to be sent. This will be stringified.
+ * @param {object} command The command describing the entity to execute the operation with
+ * @param {object} body The PATCH body that needs to be sent. This will be stringified.
  */
 export interface Patcher<Kind extends Command.Kind> {
   patch(
