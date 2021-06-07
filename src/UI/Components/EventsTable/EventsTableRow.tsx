804909d50ed2a37f96c67b3bb57e155f4ce7a2aa
Stijn Coolen <stijn.coolen@inmanta.com>
2021-06-07 13:33:45 +0200

    Refactor css for EventsRow

--- src/UI/Components/EventsTable/EventsTableRow.tsx
+++ src/UI/Components/EventsTable/EventsTableRow.tsx
@@ -1,4 +1,5 @@
 import { EventRow } from "@/Core";
+import styled from "styled-components";
 import {
   DescriptionList,
   DescriptionListDescription,
@@ -25,67 +26,68 @@ export const EventsTableRow: React.FC<Props> = ({
   isExpanded,
   onToggle,
   numberOfColumns,
-}) => {
-  const getColorCodeClass = (row) => {
-    if (row.isErrorTransition) {
-      return "error-transition";
-    } else if (row.ignoredTransition) {
-      return "ignored-transition";
-    }
-    return "";
-  };
-  return (
-    <Tbody isExpanded={false} className={getColorCodeClass(row)}>
-      <Tr id={`event-row-${row.id}`} aria-label="Event table row">
-        <Td
-          expand={{
-            rowIndex: index,
-            isExpanded,
-            onToggle,
-          }}
-        />
-        <Td>
-          <EventIcon eventType={row.eventType} />
-        </Td>
-        <Td>
-          <DateWithTooltip date={row.timestamp} />
-        </Td>
-        <Td>{row.serviceInstanceVersion}</Td>
-        <Td>{row.source}</Td>
-        <Td>{row.destination}</Td>
-      </Tr>
-      <Tr isExpanded={isExpanded} data-testid={`details_${row.id}`}>
-        <Td colSpan={numberOfColumns}>
-          <ExpandableRowContent>
-            <DescriptionList>
-              <DescriptionListGroup>
-                <DescriptionListTerm>
-                  {words("events.column.message")}
-                </DescriptionListTerm>
-                <DescriptionListDescription>
-                  {row.message}
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              <DescriptionListGroup>
-                <DescriptionListTerm>
-                  {words("events.details.title")}
-                </DescriptionListTerm>
-                <DescriptionListDescription>
-                  <pre
-                    style={{
-                      whiteSpace: "pre-wrap",
-                      fontFamily: "Liberation Mono",
-                    }}
-                  >
-                    <code>{JSON.stringify(row.fullJson, null, 2)}</code>
-                  </pre>
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              {row.idCompileReport && <CompileReportLink />}
-            </DescriptionList>
-          </ExpandableRowContent>
-        </Td>
-      </Tr>
-    </Tbody>
-  );
-};
+}) => (
+  <StyledBody isExpanded={false} transition={row}>
+    <Tr id={`event-row-${row.id}`} aria-label="Event table row">
+      <Td
+        expand={{
+          rowIndex: index,
+          isExpanded,
+          onToggle,
+        }}
+      />
+      <Td>
+        <EventIcon eventType={row.eventType} />
+      </Td>
+      <Td>
+        <DateWithTooltip date={row.timestamp} />
+      </Td>
+      <Td>{row.serviceInstanceVersion}</Td>
+      <Td>{row.source}</Td>
+      <Td>{row.destination}</Td>
+    </Tr>
+    <Tr isExpanded={isExpanded} data-testid={`details_${row.id}`}>
+      <Td colSpan={numberOfColumns}>
+        <ExpandableRowContent>
+          <DescriptionList>
+            <DescriptionListGroup>
+              <DescriptionListTerm>
+                {words("events.column.message")}
+              </DescriptionListTerm>
+              <DescriptionListDescription>
+                {row.message}
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+            <DescriptionListGroup>
+              <DescriptionListTerm>
+                {words("events.details.title")}
+              </DescriptionListTerm>
+              <DescriptionListDescription>
+                <pre
+                  style={{
+                    whiteSpace: "pre-wrap",
+                    fontFamily: "Liberation Mono",
+                  }}
+                >
+                  <code>{JSON.stringify(row.fullJson, null, 2)}</code>
+                </pre>
+              </DescriptionListDescription>
+            </DescriptionListGroup>
+            {row.idCompileReport && <CompileReportLink />}
+          </DescriptionList>
+        </ExpandableRowContent>
+      </Td>
+    </Tr>
+  </StyledBody>
+);
+
+type Transition = Pick<EventRow, "ignoredTransition" | "isErrorTransition">;
+
+const StyledBody = styled(Tbody)<{ transition: Transition }>`
+  ${({ transition }) =>
+    transition.isErrorTransition
+      ? "background-color: var(--pf-global--palette--gold-50)"
+      : transition.ignoredTransition
+      ? "background-color: var(--pf-global--palette--black-200)"
+      : ""};
+`;
