1b97e52b920f317b7cfe6b65ad8ff94d7c5ef428
Stijn Coolen <stijn.coolen@inmanta.com>
2021-07-28 16:52:47 +0200

    Rephrase test for getUrl

--- src/Data/Managers/ResourceHistory/getUrl.test.ts
+++ src/Data/Managers/ResourceHistory/getUrl.test.ts
@@ -1,28 +1,21 @@
 import { Query } from "@/Core";
 import { getUrl } from "./getUrl";
 
-describe("getUrl for latest released resources ", () => {
-  const baseQuery: Query.SubQuery<"ResourceHistory"> = {
-    kind: "ResourceHistory",
-    pageSize: 20,
-    id: "resourceId1",
-  };
-  it.each`
-    sort                               | pageSize | url
-    ${undefined}                       | ${20}    | ${`/api/v2/resource/resourceId1/history?limit=20`}
-    ${undefined}                       | ${10}    | ${`/api/v2/resource/resourceId1/history?limit=10`}
-    ${{ name: "date", order: "asc" }}  | ${10}    | ${`/api/v2/resource/resourceId1/history?limit=10&sort=date.asc`}
-    ${{ name: "date", order: "desc" }} | ${20}    | ${`/api/v2/resource/resourceId1/history?limit=20&sort=date.desc`}
-  `(
-    "returns correct url $url page size $pageSize",
-    ({ sort, pageSize, url }) => {
-      const query: Query.SubQuery<"ResourceHistory"> = {
-        ...baseQuery,
-        pageSize,
-        sort,
-      };
-
-      expect(getUrl(query)).toEqual(url);
-    }
-  );
-});
+it.each`
+  sort                               | sortTxt        | pageSize | url
+  ${undefined}                       | ${"undefined"} | ${20}    | ${`/api/v2/resource/resourceId1/history?limit=20`}
+  ${undefined}                       | ${"undefined"} | ${10}    | ${`/api/v2/resource/resourceId1/history?limit=10`}
+  ${{ name: "date", order: "asc" }}  | ${"date.asc"}  | ${10}    | ${`/api/v2/resource/resourceId1/history?limit=10&sort=date.asc`}
+  ${{ name: "date", order: "desc" }} | ${"date.desc"} | ${20}    | ${`/api/v2/resource/resourceId1/history?limit=20&sort=date.desc`}
+`(
+  "getUrl returns correct url for latest resources with sort: $sortTxt and pageSize: $pageSize",
+  ({ sort, pageSize, url }) => {
+    const query: Query.SubQuery<"ResourceHistory"> = {
+      kind: "ResourceHistory",
+      id: "resourceId1",
+      pageSize,
+      sort,
+    };
+    expect(getUrl(query)).toEqual(url);
+  }
+);
