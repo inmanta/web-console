f385c776048010fa0db84dc5004763a9584aaac8
Stijn Coolen <stijn.coolen@inmanta.com>
2021-08-06 00:31:51 +0200

    Fetch data & Show data in tab

--- src/UI/Pages/Resources/Tabs/AttributesTab/AttributesTab.tsx
+++ src/UI/Pages/Resources/Tabs/AttributesTab/AttributesTab.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RemoteData } from "@/Core";
+import { ErrorView, LoadingView } from "@/UI/Components";
+import { DependencyContext } from "@/UI/Dependency";
+import { words } from "@/UI/words";
 
 interface Props {
   id: string;
 }
 
-export const AttributesTab: React.FC<Props> = ({ id }) => (
-  <div>desired state tab: {id}</div>
-);
+export const AttributesTab: React.FC<Props> = ({ id }) => {
+  const { queryResolver } = useContext(DependencyContext);
+
+  const [data] = queryResolver.useContinuous<"ResourceDetails">({
+    kind: "ResourceDetails",
+    id,
+  });
+
+  return RemoteData.fold(
+    {
+      notAsked: () => null,
+      loading: () => (
+        <LoadingView delay={500} aria-label="ResourceAttributes-Loading" />
+      ),
+      failed: (error) => (
+        <ErrorView
+          aria-label="ResourceAttributes-Failed"
+          title={words("resources.details.failed.title")}
+          message={words("resources.details.failed.body")(error)}
+        />
+      ),
+      success: (resource) => (
+        <pre>
+          <code>{JSON.stringify(resource, null, 4)}</code>
+        </pre>
+      ),
+    },
+    data
+  );
+};
