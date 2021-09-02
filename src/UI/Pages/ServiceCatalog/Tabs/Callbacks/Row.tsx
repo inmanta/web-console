215eb5c9de780a928f5edde9210b995e453284e9
Stijn Coolen <stijn.coolen@inmanta.com>
2021-09-02 17:45:16 +0200

    Add DeleteButton

--- src/UI/Pages/ServiceCatalog/Tabs/Callbacks/Row.tsx
+++ src/UI/Pages/ServiceCatalog/Tabs/Callbacks/Row.tsx
@@ -1,9 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Callback } from "@/Core";
 import { Tbody, Td, Tr, ExpandableRowContent } from "@patternfly/react-table";
-import { Button } from "@patternfly/react-core";
-import { TrashAltIcon } from "@patternfly/react-icons";
-import { DependencyContext } from "@/UI/Dependency";
+import { DeleteButton } from "./DeleteButton";
 
 interface Props {
   callback: Callback;
@@ -22,13 +20,6 @@ export const Row: React.FC<Props> = ({
   onToggle,
   numberOfColumns,
 }) => {
-  const { commandResolver } = useContext(DependencyContext);
-  const onDelete = commandResolver.getTrigger<"DeleteCallback">({
-    kind: "DeleteCallback",
-    callbackId: callback.callback_id,
-    service_entity,
-  });
-
   return (
     <Tbody isExpanded={false}>
       <Tr>
@@ -42,12 +33,7 @@ export const Row: React.FC<Props> = ({
         <Td>{callback.url}</Td>
         <Td>{callback.callback_id}</Td>
         <Td>
-          <Button
-            variant="secondary"
-            isDanger
-            icon={<TrashAltIcon />}
-            onClick={onDelete}
-          />
+          <DeleteButton callback={callback} service_entity={service_entity} />
         </Td>
       </Tr>
       {isExpanded && (
--- src/UI/words.ts
+++ src/UI/words.ts
@@ -189,6 +189,8 @@ const dict = {
   "catalog.delete.title": (serviceName: string) =>
     `Are you sure you want to delete service entity ${serviceName}?`,
   "catalog.instances": "Instances",
+  "catalog.callbacks.delete": (url: string) =>
+    `Are you sure you want to delete callback with url "${url}"?`,
 
   /**
    * Environment controls
