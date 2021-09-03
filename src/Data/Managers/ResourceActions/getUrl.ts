99325fab2feb738f105bdc791ed9794b7cc2ed6b
Stijn Coolen <stijn.coolen@inmanta.com>
2021-09-03 11:50:16 +0200

    Add limit

--- src/Data/Managers/ResourceActions/getUrl.ts
+++ src/Data/Managers/ResourceActions/getUrl.ts
@@ -6,5 +6,5 @@ export function getUrl({
   resource_type,
   resource_id_value,
 }: Query.SubQuery<"ResourceActions">): string {
-  return `/api/v2/resource_actions?agent=${agent}&attribute=${attribute}&resource_type=${resource_type}&attribute_value=${resource_id_value}`;
+  return `/api/v2/resource_actions?agent=${agent}&attribute=${attribute}&resource_type=${resource_type}&attribute_value=${resource_id_value}&limit=20`;
 }
