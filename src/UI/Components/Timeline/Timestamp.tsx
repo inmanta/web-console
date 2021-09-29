2f65b7265ef1795c821c08184654677aff7e498a
Stijn Coolen <stijn.coolen@inmanta.com>
2021-09-29 09:19:06 +0200

    Add Timeline component (Issue #976, PR #1443)
    # Description
    
    <img width="634" alt="Screenshot 2021-09-28 at 19 41 04" src="https://user-images.githubusercontent.com/2602506/135138664-89153cda-4896-4c6c-b81e-5898ddabe315.png">
    <img width="618" alt="Screenshot 2021-09-28 at 19 41 15" src="https://user-images.githubusercontent.com/2602506/135138666-ee3593d0-9337-491c-8f2c-15c2dc865084.png">
    <img width="644" alt="Screenshot 2021-09-28 at 19 41 24" src="https://user-images.githubusercontent.com/2602506/135138668-78083d72-13f9-4d9e-9815-475592093b83.png">
    
    relates to #976
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

---
+++ src/UI/Components/Timeline/Timestamp.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface Timestamp {
+  day: string;
+  time: string;
+}
+
+const TimestampLabel: React.FC<{ timestamp: Timestamp; className?: string }> =
+  ({ timestamp, className }) => (
+    <TimestampContainer className={className}>
+      <TimeLabel>{timestamp.time}</TimeLabel>
+      <DayLabel>{timestamp.day}</DayLabel>
+    </TimestampContainer>
+  );
+
+const TimeLabel = styled.p`
+  display: inline-block;
+  font-weight: 600;
+`;
+
+const DayLabel = styled.p`
+  display: inline-block;
+  font-weight: 400;
+`;
+
+const TimestampContainer = styled.div`
+  position: absolute;
+  font-size: 14px;
+  bottom: 8px;
+  text-align: center;
+  width: 100px;
+`;
+
+export const RequestedTimestampLabel = styled(TimestampLabel)`
+  left: 65px;
+  transform: translateX(-50%);
+`;
+
+export const StartedTimestampLabel = styled(TimestampLabel)`
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+export const CompletedTimestampLabel = styled(TimestampLabel)`
+  right: 65px;
+  transform: translateX(50%);
+`;
