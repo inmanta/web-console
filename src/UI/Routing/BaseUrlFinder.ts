d43983d14750c7cb4d8ad33a22746c9d765d40c3
Stijn Coolen <stijn.coolen@inmanta.com>
2021-10-20 13:39:36 +0200

    Cleanup Anchor

--- src/UI/Routing/BaseUrlFinder.ts
+++ src/UI/Routing/BaseUrlFinder.ts
@@ -4,7 +4,7 @@ export class BaseUrlFinder {
   getUrl(url: string): string {
     if (!url.includes(ANCHOR)) return ANCHOR;
     if (url.split(ANCHOR).length > 2) return ANCHOR;
-    const [firstPart] = url.split("/console");
-    return `${firstPart}${ANCHOR}`;
+    const [pre] = url.split(ANCHOR);
+    return `${pre}${ANCHOR}`;
   }
 }
