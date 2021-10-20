f3a34d0b48d399c1ce7bb3216ad72137bac00130
Stijn Coolen <stijn.coolen@inmanta.com>
2021-10-20 15:50:26 +0200

    Fix crumb test

--- src/UI/Routing/Crumb.test.ts
+++ src/UI/Routing/Crumb.test.ts
@@ -1,100 +1,100 @@
 import { getCrumbs } from "./Crumb";
 
-test("GIVEN '/lsm/catalog' THEN breadcrumbs should be ['Home','Catalog']", () => {
-  const crumbs = getCrumbs("/lsm/catalog");
+test("GIVEN '/console/lsm/catalog' THEN breadcrumbs should be ['Home','Catalog']", () => {
+  const crumbs = getCrumbs("/console/lsm/catalog");
   expect(crumbs).toHaveLength(2);
   expect(crumbs).toEqual([
     {
       kind: "Home",
       label: "Home",
-      url: "/",
+      url: "/console/",
       active: false,
     },
     {
       kind: "Catalog",
       label: "Service Catalog",
-      url: "/lsm/catalog",
+      url: "/console/lsm/catalog",
       active: true,
     },
   ]);
 });
 
-test("GIVEN '/lsm/catalog/xyz/inventory' THEN breadcrumbs should be ['Home', 'Catalog', 'Inventory']", () => {
-  const crumbs = getCrumbs("/lsm/catalog/xyz/inventory");
+test("GIVEN '/console/lsm/catalog/xyz/inventory' THEN breadcrumbs should be ['Home', 'Catalog', 'Inventory']", () => {
+  const crumbs = getCrumbs("/console/lsm/catalog/xyz/inventory");
   expect(crumbs).toHaveLength(3);
   expect(crumbs).toEqual([
     {
       kind: "Home",
       label: "Home",
-      url: "/",
+      url: "/console/",
       active: false,
     },
     {
       kind: "Catalog",
       label: "Service Catalog",
-      url: "/lsm/catalog",
+      url: "/console/lsm/catalog",
       active: false,
     },
     {
       kind: "Inventory",
       label: "Service Inventory",
-      url: "/lsm/catalog/xyz/inventory",
+      url: "/console/lsm/catalog/xyz/inventory",
       active: true,
     },
   ]);
 });
 
-test("GIVEN '/lsm/catalog/xyz/inventory/123/history' THEN breadcrumbs should be ['Home','Catalog', 'Inventory', 'History']", () => {
-  const crumbs = getCrumbs("/lsm/catalog/xyz/inventory/123/history");
+test("GIVEN '/console/lsm/catalog/xyz/inventory/123/history' THEN breadcrumbs should be ['Home','Catalog', 'Inventory', 'History']", () => {
+  const crumbs = getCrumbs("/console/lsm/catalog/xyz/inventory/123/history");
   expect(crumbs).toHaveLength(4);
   expect(crumbs).toEqual([
     {
       kind: "Home",
       label: "Home",
-      url: "/",
+      url: "/console/",
       active: false,
     },
     {
       kind: "Catalog",
       label: "Service Catalog",
-      url: "/lsm/catalog",
+      url: "/console/lsm/catalog",
       active: false,
     },
     {
       kind: "Inventory",
       label: "Service Inventory",
-      url: "/lsm/catalog/xyz/inventory",
+      url: "/console/lsm/catalog/xyz/inventory",
       active: false,
     },
     {
       kind: "History",
       label: "Service Instance History",
-      url: "/lsm/catalog/xyz/inventory/123/history",
+      url: "/console/lsm/catalog/xyz/inventory/123/history",
       active: true,
     },
   ]);
 });
 
-test("GIVEN '/resources/123' THEN breadcrumbs should be ['Home', 'Resources', 'Resource Details']", () => {
-  const crumbs = getCrumbs("/resources/123");
+test("GIVEN '/console/resources/123' THEN breadcrumbs should be ['Home', 'Resources', 'Resource Details']", () => {
+  const crumbs = getCrumbs("/console/resources/123");
   expect(crumbs).toHaveLength(3);
   expect(crumbs).toEqual([
     {
       kind: "Home",
       label: "Home",
-      url: "/",
+      url: "/console/",
       active: false,
     },
     {
       kind: "Resources",
       label: "Resources",
-      url: "/resources",
+      url: "/console/resources",
       active: false,
     },
     {
       kind: "ResourceDetails",
       label: "Resource Details",
-      url: "/resources/123",
+      url: "/console/resources/123",
       active: true,
     },
   ]);
