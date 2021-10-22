13f77770eeebf37c57508c1081ce37b9cb4cfa2f
Stijn Coolen <stijn.coolen@inmanta.com>
2021-10-22 16:08:26 +0200

    Restructure input files

--- src/UI/Pages/Settings/Tabs/Configuration/Input.tsx
+++ src/UI/Pages/Settings/Tabs/Configuration/Input.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import styled from "styled-components";
+import { NumberInput, Switch } from "@patternfly/react-core";
 import { EnvironmentSettings } from "@/Core";
-import { IntInput } from "./IntInput";
-import { BooleanInput } from "./BooleanInput";
-import { EnumInput } from "./EnumInput";
+import { SingleTextSelect } from "@/UI/Components";
 
 interface Props {
   info: EnvironmentSettings.InputInfo;
@@ -20,3 +20,50 @@ export const Input: React.FC<Props> = ({ info }) => {
       return <>dict</>;
   }
 };
+
+const EnumInput: React.FC<{ info: EnvironmentSettings.EnumInputInfo }> = ({
+  info,
+}) => {
+  const setSelected = (value) => (value !== null ? info.set(value) : undefined);
+  return (
+    <StyledSingleTextSelect
+      selected={info.value}
+      setSelected={setSelected}
+      options={info.allowed_values}
+    />
+  );
+};
+
+const StyledSingleTextSelect = styled(SingleTextSelect)`
+  width: 300px;
+`;
+
+const IntInput: React.FC<{ info: EnvironmentSettings.IntInputInfo }> = ({
+  info,
+}) => {
+  const onChange = (event) => {
+    info.set(Number(event.target.value));
+  };
+  const onMinus = () => info.set(info.value - 1);
+  const onPlus = () => info.set(info.value + 1);
+
+  return (
+    <NumberInput
+      value={info.value}
+      onMinus={onMinus}
+      onChange={onChange}
+      onPlus={onPlus}
+      inputName="input"
+      inputAriaLabel="number input"
+      minusBtnAriaLabel="minus"
+      plusBtnAriaLabel="plus"
+      widthChars={10}
+    />
+  );
+};
+
+export const BooleanInput: React.FC<{
+  info: EnvironmentSettings.BooleanInputInfo;
+}> = ({ info }) => (
+  <Switch isChecked={info.value} onChange={info.set} aria-label={info.name} />
+);
