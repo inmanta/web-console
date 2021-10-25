ec65a4e6cea3777e7e574ef3ed5ce28346c46558
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2021-10-25 14:15:08 +0200

    Use simple form inputs instead of inline editable ones on create environment page (Issue #1736, PR #1742)
    # Description
    
    closes #1736
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] ~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

---
+++ src/UI/Pages/CreateEnvironment/SimpleTextFormInput.tsx
@@ -0,0 +1,26 @@
+import { FormGroup, TextInput } from "@patternfly/react-core";
+import React from "react";
+
+interface Props {
+  label: string;
+  isRequired?: boolean;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const SimpleTextFormInput: React.FC<Props> = ({
+  label,
+  isRequired,
+  value,
+  onChange,
+}) => {
+  return (
+    <FormGroup fieldId={label} label={label} isRequired={isRequired}>
+      <TextInput
+        aria-label={`${label}-input`}
+        value={value}
+        onChange={onChange}
+      />
+    </FormGroup>
+  );
+};
