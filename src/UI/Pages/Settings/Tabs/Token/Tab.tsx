544e6ddc961038097dabb853342485da5dee0cc8
Stijn Coolen <stijn.coolen@inmanta.com>
2021-11-05 14:41:06 +0100

    Add test

--- src/UI/Pages/Settings/Tabs/Token/Tab.tsx
+++ src/UI/Pages/Settings/Tabs/Token/Tab.tsx
@@ -1,25 +1,7 @@
 import React, { useContext, useState } from "react";
 import { ClientType, Either, Maybe, toggleValueInList } from "@/Core";
-import { ClipboardCopyButton, Description } from "@/UI/Components";
-import { words } from "@/UI/words";
-import styled from "styled-components";
-import {
-  Alert,
-  AlertActionCloseButton,
-  Button,
-  Flex,
-  FlexItem,
-  InputGroup,
-  TextInput,
-  ToggleGroup,
-  ToggleGroupItem,
-} from "@patternfly/react-core";
-import {
-  ClusterIcon,
-  ProcessAutomationIcon,
-  UserIcon,
-} from "@patternfly/react-icons";
 import { DependencyContext } from "@/UI";
+import { TokenForm } from "./TokenForm";
 
 export const Tab: React.FC = () => {
   const { commandResolver } = useContext(DependencyContext);
@@ -59,87 +41,14 @@ export const Tab: React.FC = () => {
   };
 
   return (
-    <Container>
-      <Description>{words("settings.tabs.token.description")}</Description>
-      <PaddedFlex>
-        <FlexItem>
-          <ToggleGroup aria-label="ClientTypes">
-            <ToggleGroupItem
-              icon={<UserIcon />}
-              text="agent"
-              aria-label="AgentOption"
-              isSelected={isClientTypeSelected("agent")}
-              onChange={getClientTypeSelector("agent")}
-              isDisabled={isBusy}
-            />
-            <ToggleGroupItem
-              icon={<ClusterIcon />}
-              text="api"
-              aria-label="ApiOption"
-              isSelected={isClientTypeSelected("api")}
-              onChange={getClientTypeSelector("api")}
-              isDisabled={isBusy}
-            />
-            <ToggleGroupItem
-              icon={<ProcessAutomationIcon />}
-              text="compiler"
-              aria-label="CompilerOption"
-              isSelected={isClientTypeSelected("compiler")}
-              onChange={getClientTypeSelector("compiler")}
-              isDisabled={isBusy}
-            />
-          </ToggleGroup>
-        </FlexItem>
-        <FlexItem>
-          <Button variant="primary" onClick={onGenerate} isDisabled={isBusy}>
-            {words("settings.tabs.token.generate")}
-          </Button>
-        </FlexItem>
-      </PaddedFlex>
-
-      {Maybe.isSome(error) && (
-        <Alert
-          isInline
-          variant="danger"
-          title="Something went wrong"
-          actionClose={
-            <AlertActionCloseButton onClose={() => setError(Maybe.none())} />
-          }
-        >
-          <p>{error.value}</p>
-        </Alert>
-      )}
-
-      <PaddedInputGroup>
-        <TextInput
-          name="token"
-          id="token"
-          type="text"
-          aria-label="token"
-          isDisabled={Maybe.isNone(token)}
-          value={Maybe.withFallback(token, "")}
-        />
-        <ClipboardCopyButton
-          value={Maybe.withFallback(token, "")}
-          isDisabled={Maybe.isNone(token)}
-          variant="control"
-          aria-label="CopyTokenToClipboard"
-        />
-      </PaddedInputGroup>
-    </Container>
+    <TokenForm
+      onGenerate={onGenerate}
+      onErrorClose={() => setError(Maybe.none())}
+      getClientTypeSelector={getClientTypeSelector}
+      isClientTypeSelected={isClientTypeSelected}
+      token={token}
+      error={error}
+      isBusy={isBusy}
+    />
   );
 };
-
-const PaddedInputGroup = styled(InputGroup)`
-  padding-top: 1rem;
-  max-width: 600px;
-`;
-
-const Container = styled.div`
-  padding-top: 1rem;
-`;
-
-const PaddedFlex = styled(Flex)`
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-`;
