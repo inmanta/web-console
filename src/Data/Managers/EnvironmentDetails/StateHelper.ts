e1512000f623eb01339ff6a9864fa73aa1dec3fb
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-10 14:54:26 +0100

    Refactor StateHelper

--- src/Data/Managers/EnvironmentDetails/StateHelper.ts
+++ src/Data/Managers/EnvironmentDetails/StateHelper.ts
@@ -1,73 +1,29 @@
-import { isEqual } from "lodash";
-import { Query, RemoteData, StateHelper } from "@/Core";
-import { Store, useStoreState } from "@/Data/Store";
+import { Query, RemoteData } from "@/Core";
+import { PrimaryStateHelper } from "@/Data/Common";
+import { Store, State, Dispatch } from "@/Data/Store";
 
-type Data = RemoteData.Type<
-  Query.Error<"GetEnvironmentDetails">,
-  Query.Data<"GetEnvironmentDetails">
->;
-type ApiData = RemoteData.Type<
-  Query.Error<"GetEnvironmentDetails">,
-  Query.ApiResponse<"GetEnvironmentDetails">
->;
+export class EnvironmentDetailsStateHelper extends PrimaryStateHelper<"GetEnvironmentDetails"> {
+  constructor(store: Store, private readonly environment: string) {
+    super(
+      store,
+      (data, query) => {
+        const unwrapped = RemoteData.mapSuccess(
+          (wrapped) => wrapped.data,
+          data
+        );
+        this.getSlice(store.dispatch, query).setData({
+          id: this.environment,
+          value: unwrapped,
+        });
+      },
+      (state, query) => this.getSlice(state, query).byEnv[environment]
+    );
+  }
 
-export class EnvironmentDetailsStateHelper
-  implements StateHelper<"GetEnvironmentDetails">
-{
-  constructor(
-    private readonly store: Store,
-    private readonly environment: string
-  ) {}
-
-  set(
-    data: ApiData,
+  private getSlice<T extends Dispatch | State>(
+    root: T,
     { details }: Query.SubQuery<"GetEnvironmentDetails">
-  ): void {
-    const unwrapped = RemoteData.mapSuccess((wrapped) => wrapped.data, data);
-    if (details) {
-      this.store.dispatch.environmentDetailsWithIcon.setData({
-        id: this.environment,
-        value: unwrapped,
-      });
-    } else {
-      this.store.dispatch.environmentDetails.setData({
-        id: this.environment,
-        value: unwrapped,
-      });
-    }
-  }
-
-  getHooked({ details }: Query.SubQuery<"GetEnvironmentDetails">): Data {
-    if (details) {
-      /* eslint-disable-next-line react-hooks/rules-of-hooks */
-      return useStoreState(
-        (state) =>
-          this.enforce(
-            state.environmentDetailsWithIcon.byEnv[this.environment]
-          ),
-        isEqual
-      );
-    }
-    /* eslint-disable-next-line react-hooks/rules-of-hooks */
-    return useStoreState(
-      (state) => this.enforce(state.environmentDetails.byEnv[this.environment]),
-      isEqual
-    );
-  }
-
-  private enforce(value: undefined | Data): Data {
-    if (typeof value === "undefined") return RemoteData.notAsked();
-    return value;
-  }
-
-  getOnce({ details }: Query.SubQuery<"GetEnvironmentDetails">): Data {
-    if (details) {
-      return this.enforce(
-        this.store.getState().environmentDetailsWithIcon.byEnv[this.environment]
-      );
-    }
-    return this.enforce(
-      this.store.getState().environmentDetails.byEnv[this.environment]
-    );
+  ): T["environmentDetails" | "environmentDetailsWithIcon"] {
+    return details ? root.environmentDetailsWithIcon : root.environmentDetails;
   }
 }
