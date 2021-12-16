2b39c2b2b0ee2e904b7c8487ad17fad432aa839b
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-16 14:30:40 +0100

    Extract axe setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import "@patternfly/react-core/dist/styles/base.css";
+import "./setup";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -6,12 +7,6 @@ import { StoreProvider } from "easy-peasy";
 import { getStoreInstance } from "@/Data";
 import { Root } from "@/UI/Root";
 import { Injector } from "./Injector";
-
-if (process.env.NODE_ENV !== "production") {
-  /* eslint-disable-next-line @typescript-eslint/no-var-requires */
-  const axe = require("react-axe");
-  axe(React, ReactDOM, 1000);
-}
 
 const store = getStoreInstance();
 
