b400840850bbd1894a8805e7cd575fd1344d7a4f
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-21 18:15:35 +0100

    Refactor CompileDetails StateHelper

--- src/Data/Managers/CompileDetails/StateHelper.ts
+++ src/Data/Managers/CompileDetails/StateHelper.ts
@@ -1,33 +1,16 @@
-import { isEqual } from "lodash";
-import { Query, RemoteData, StateHelper, CompileDetails } from "@/Core";
-import { Store, useStoreState } from "@/Data/Store";
+import { RemoteData } from "@/Core";
+import { PrimaryStateHelper } from "@/Data/Common";
+import { Store } from "@/Data/Store";
 
-type Data = RemoteData.Type<string, CompileDetails>;
-type ApiData = RemoteData.Type<string, Query.ApiResponse<"GetCompileDetails">>;
-
-export class CompileDetailsStateHelper
-  implements StateHelper<"GetCompileDetails">
-{
-  constructor(private readonly store: Store) {}
-
-  set(data: ApiData, { id }: Query.SubQuery<"GetCompileDetails">): void {
-    const value = RemoteData.mapSuccess((data) => data.data, data);
-    this.store.dispatch.compileDetails.setData({ id, value });
-  }
-
-  getHooked({ id }: Query.SubQuery<"GetCompileDetails">): Data {
-    /* eslint-disable-next-line react-hooks/rules-of-hooks */
-    return useStoreState((state) => {
-      return this.enforce(state.compileDetails.byId[id]);
-    }, isEqual);
-  }
-
-  private enforce(value: undefined | Data): Data {
-    if (typeof value === "undefined") return RemoteData.notAsked();
-    return value;
-  }
-
-  getOnce({ id }: Query.SubQuery<"GetCompileDetails">): Data {
-    return this.enforce(this.store.getState().compileDetails.byId[id]);
+export class CompileDetailsStateHelper extends PrimaryStateHelper<"GetCompileDetails"> {
+  constructor(store: Store) {
+    super(
+      store,
+      (data, { id }) => {
+        const value = RemoteData.mapSuccess((data) => data.data, data);
+        store.dispatch.compileDetails.setData({ id, value });
+      },
+      (state, { id }) => state.compileDetails.byId[id]
+    );
   }
 }
