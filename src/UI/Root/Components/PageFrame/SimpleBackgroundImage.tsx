7da75c8346e4e1eda193eac84c46c112078aa6b5
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-21 15:26:03 +0100

    Refactor SimpleBackgroundImage

--- src/UI/Root/Components/PageFrame/SimpleBackgroundImage.tsx
+++ src/UI/Root/Components/PageFrame/SimpleBackgroundImage.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { BackgroundImage } from "@patternfly/react-core";
-import filter from "@patternfly/react-core/dist/styles/assets/images/background-filter.svg";
 
-export class SimpleBackgroundImage extends React.Component {
-  private images = {
-    filter: `${filter}#image_overlay`,
-    lg: `pfbg_2000.jpg`,
-    sm: `pfbg_768.jpg`,
-    sm2x: `pfbg_768@2x.jpg`,
-    xs: `pfbg_576.jpg`,
-    xs2x: `pfbg_576@2x.jpg`,
-  };
-
-  constructor(props: Record<string, unknown>) {
-    super(props);
-  }
-
-  public render(): JSX.Element {
-    return <BackgroundImage src={this.images} alt="Background image" />;
-  }
-}
+export const SimpleBackgroundImage: React.FC = () => (
+  <BackgroundImage
+    src={{
+      lg: `pfbg_2000.jpg`,
+      sm: `pfbg_768.jpg`,
+      sm2x: `pfbg_768@2x.jpg`,
+      xs: `pfbg_576.jpg`,
+      xs2x: `pfbg_576@2x.jpg`,
+    }}
+    alt="Background image"
+  />
+);
