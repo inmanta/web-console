7ed44b552346469e9596a4421ab6e4eb00e57667
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-22 14:22:34 +0100

    Refactor CommandManager for CreateProject

--- src/Data/Managers/CreateProject/CommandManager.ts
+++ src/Data/Managers/CreateProject/CommandManager.ts
@@ -1,35 +1,25 @@
-import { ApiHelper, Command, CommandManager, Maybe, Updater } from "@/Core";
+import { ApiHelper, Maybe, Updater } from "@/Core";
+import { CommandManagerWithoutEnv } from "@/Data";
 
-export class CreateProjectCommandManager implements CommandManager {
+export class CreateProjectCommandManager extends CommandManagerWithoutEnv<"CreateProject"> {
   constructor(
     private readonly apiHelper: ApiHelper,
     private readonly updater: Updater<"GetProjects">
-  ) {}
-
-  matches(command: Command.SubCommand<"CreateProject">): boolean {
-    return command.kind === "CreateProject";
-  }
-
-  getTrigger(
-    command: Command.SubCommand<"CreateProject">
-  ): Command.Trigger<"CreateProject"> {
-    return (name: string) => this.submit(command, { name });
-  }
-
-  private async submit(
-    command: Command.SubCommand<"CreateProject">,
-    body: { name: string }
-  ): Promise<Maybe.Type<Command.Error<"CreateProject">>> {
-    const result = await this.apiHelper.putWithoutResponseAndEnvironment(
-      `/api/v2/project`,
-      body
-    );
-    if (Maybe.isNone(result)) {
-      await this.updater.update({
-        kind: "GetProjects",
-        environmentDetails: false,
-      });
-    }
-    return result;
+  ) {
+    super("CreateProject", () => {
+      return async (name: string) => {
+        const result = await this.apiHelper.putWithoutResponseAndEnvironment(
+          `/api/v2/project`,
+          { name }
+        );
+        if (Maybe.isNone(result)) {
+          await this.updater.update({
+            kind: "GetProjects",
+            environmentDetails: false,
+          });
+        }
+        return result;
+      };
+    });
   }
 }
