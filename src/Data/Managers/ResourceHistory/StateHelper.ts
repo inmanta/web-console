8dbb081a5a04da9026e1df0e0ab2798b2df39f4d
Stijn Coolen <stijn.coolen@inmanta.com>
2021-12-23 11:26:13 +0100

    Refactor ResourceHistory

--- src/Data/Managers/ResourceHistory/StateHelper.ts
+++ src/Data/Managers/ResourceHistory/StateHelper.ts
@@ -1,43 +1,19 @@
-import { isEqual } from "lodash";
-import { Query, RemoteData, StateHelper } from "@/Core";
-import { Store, useStoreState } from "@/Data/Store";
+import { RemoteData } from "@/Core";
+import { PrimaryStateHelper } from "@/Data/Common";
+import { Store } from "@/Data/Store";
 
-type Data = RemoteData.Type<
-  Query.Error<"GetResourceHistory">,
-  Query.Data<"GetResourceHistory">
->;
-type ApiData = RemoteData.Type<
-  Query.Error<"GetResourceHistory">,
-  Query.ApiResponse<"GetResourceHistory">
->;
-
-export class ResourceHistoryStateHelper
-  implements StateHelper<"GetResourceHistory">
-{
-  constructor(private readonly store: Store) {}
-
-  set(data: ApiData, query: Query.SubQuery<"GetResourceHistory">): void {
-    const value = RemoteData.mapSuccess((data) => data, data);
-    this.store.dispatch.resourceHistory.setData({
-      id: query.id,
-      value,
-    });
-  }
-
-  getHooked(query: Query.SubQuery<"GetResourceHistory">): Data {
-    /* eslint-disable-next-line react-hooks/rules-of-hooks */
-    return useStoreState(
-      (state) => this.enforce(state.resourceHistory.byId[query.id]),
-      isEqual
+export class ResourceHistoryStateHelper extends PrimaryStateHelper<"GetResourceHistory"> {
+  constructor(store: Store) {
+    super(
+      store,
+      (data, query) => {
+        const value = RemoteData.mapSuccess((data) => data, data);
+        store.dispatch.resourceHistory.setData({
+          id: query.id,
+          value,
+        });
+      },
+      (state, query) => state.resourceHistory.byId[query.id]
     );
-  }
-
-  private enforce(value: undefined | Data): Data {
-    if (typeof value === "undefined") return RemoteData.notAsked();
-    return value;
-  }
-
-  getOnce(query: Query.SubQuery<"GetResourceHistory">): Data {
-    return this.enforce(this.store.getState().resourceHistory.byId[query.id]);
   }
 }
