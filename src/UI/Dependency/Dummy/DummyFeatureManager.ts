915858e7bdc6d7f4922e1784cd45de9eb9bd1158
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-01-17 14:59:56 +0100

    Fix link to OSS documentation

--- src/UI/Dependency/Dummy/DummyFeatureManager.ts
+++ src/UI/Dependency/Dummy/DummyFeatureManager.ts
@@ -1,13 +1,19 @@
 import { FeatureManager } from "@/Core";
 
 export class DummyFeatureManager implements FeatureManager {
+  getServerVersion(): string {
+    throw new Error("Method not implemented.");
+  }
+  getEdition(): string {
+    throw new Error("Method not implemented.");
+  }
   isSupportEnabled(): boolean {
     throw new Error("Method not implemented.");
   }
   isLsmEnabled(): boolean {
     throw new Error("Method not implemented.");
   }
-  getServerVersion(): string {
+  getServerMajorVersion(): string {
     throw new Error("Method not implemented.");
   }
 }
