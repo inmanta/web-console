f59aa21fff6f3b4ffaf0eb086dddc8ed41d6d6fa
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-01-27 10:42:28 +0100

    Fix card headers on the diagnose page (PR #2536)
    # Description
    
    Reported on slack
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] ~Attached issue to pull request~
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] ~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/UI/words.ts
+++ src/UI/words.ts
@@ -188,11 +188,11 @@ const dict = {
 
   "diagnose.empty": (instanceId: string) =>
     `No errors were found for instance ${instanceId}`,
-  "diagnose.failure.title": "Validation failure",
+  "diagnose.failure.title": "Deployment failure",
   "diagnose.links.resourceDetails": "Resource Details",
   "diagnose.links.modelVersionDetails": "Model Version Details",
   "diagnose.links.compileReport": "Compile Report",
-  "diagnose.rejection.title": "Deployment failure",
+  "diagnose.rejection.title": "Validation failure",
   "diagnose.rejection.traceback": "Show full traceback",
   "diagnose.main.subtitle": (instanceId: string) =>
     `The following errors were found related to instance ${instanceId}`,
