9281fc94d9404d39fe7dfc13006631bb469a40d2
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-02-07 09:08:30 +0100

    Use DeferredApiHelper in test case

--- src/Data/API/FileFetcherImpl.test.ts
+++ src/Data/API/FileFetcherImpl.test.ts
@@ -1,12 +1,21 @@
-import { BaseApiHelper } from "./BaseApiHelper";
+import { DeferredApiHelper } from "@/Test";
 import { FileFetcherImpl } from "./FileFetcherImpl";
 
 test("GIVEN FileFetcher WHEN executing get THEN the result is correctly returned", async () => {
-  const fileFetcher = new FileFetcherImpl(new BaseApiHelper(), "env");
-  fetchMock.mockResponse(`{"content": "abcdefgh"}`);
-  const response = await fileFetcher.get("id1");
-  if (response.kind === "Left") {
-    throw Error("Fetching file resulted in an error");
-  }
-  expect(response.value).toEqual("abcdefgh");
+  const apiHelper = new DeferredApiHelper();
+  const fileFetcher = new FileFetcherImpl(apiHelper, "env");
+  fileFetcher.get("id1").then((response) => {
+    if (response.kind === "Left") {
+      throw Error("Fetching file resulted in an error");
+    }
+    expect(response.value).toEqual("abcdefgh");
+  });
+
+  expect(apiHelper.pendingRequests).toHaveLength(1);
+  expect(apiHelper.pendingRequests[0]).toEqual({
+    method: "GET",
+    environment: "env",
+    url: "/api/v1/file/id1",
+  });
+  apiHelper.resolve({ content: "abcdefgh" });
 });
