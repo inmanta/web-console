5d4e97d1036fce485cbf464fd6f81f94701b976b
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-02-10 17:32:11 +0100

    Encode path parameters for facts query (Issue #2623, PR #2671)

--- src/Data/Managers/ResourceDetails/getUrl.ts
+++ src/Data/Managers/ResourceDetails/getUrl.ts
@@ -2,5 +2,4 @@ import { Query } from "@/Core";
 
 type Query = Pick<Query.SubQuery<"GetResourceDetails">, "id">;
 
-export const getUrl = ({ id }: Query) =>
-  `/api/v2/resource/${encodeURIComponent(id)}`;
+export const getUrl = ({ id }: Query) => `/api/v2/resource/${id}`;
