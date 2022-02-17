e2411dcecc88527538f33494e4e871e5cab70b7a
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-02-17 13:41:31 +0100

    Use different syntax for storybook

--- src/UI/Pages/ResourceDetails/Tabs/LogTab/Details.stories.tsx
+++ src/UI/Pages/ResourceDetails/Tabs/LogTab/Details.stories.tsx
@@ -1,6 +1,5 @@
-import React, { ComponentProps } from "react";
-import { Story } from "@storybook/react/types-6-0";
-import { LogLevelString } from "@/Core";
+import React from "react";
+import { LogLevelString, ResourceLog } from "@/Core";
 import { Details } from "./Details";
 
 export default {
@@ -8,22 +7,17 @@ export default {
   component: Details,
 };
 
-const Template: Story<ComponentProps<typeof Details>> = (args) => (
-  <Details {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  log: {
-    level: LogLevelString.ERROR,
-    msg: "Failed to load handler code or install handler code dependencies. Check the agent log for details.",
-    args: [],
-    kwargs: {
-      exc_info: "true",
-      traceback: "error with tabs \t here and\n new\nlines",
-    },
-    timestamp: "2021-09-29T09:09:52.915172",
-    action_id: "6eaedb6e-1a5a-4716-824e-ce3195ddaf81",
-    action: "getfact",
+const log: ResourceLog = {
+  level: LogLevelString.ERROR,
+  msg: "Failed to load handler code or install handler code dependencies. Check the agent log for details.",
+  args: [],
+  kwargs: {
+    exc_info: "true",
+    traceback: "error with tabs \t here and\n new\nlines",
   },
+  timestamp: "2021-09-29T09:09:52.915172",
+  action_id: "6eaedb6e-1a5a-4716-824e-ce3195ddaf81",
+  action: "getfact",
 };
+
+export const Default = () => <Details log={log} />;
