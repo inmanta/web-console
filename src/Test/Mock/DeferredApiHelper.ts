75d695d717a29a1f7136f8684380c7a858c7f9e1
Stijn Coolen <stijn.coolen@inmanta.com>
2022-02-24 17:08:45 +0100

    Extend the DeferredApiHelper with ignore and resolve methods (Issue #2495, PR #2855)
    # Description
    
    closes #2495
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/Test/Mock/DeferredApiHelper.ts
+++ src/Test/Mock/DeferredApiHelper.ts
@@ -1,3 +1,4 @@
+import { isEqual, remove } from "lodash-es";
 import { ApiHelper, Deferred, Either, Maybe } from "@/Core";
 
 type Request =
@@ -27,24 +28,66 @@ interface PendingRequest {
   promise: Promise<unknown>;
 }
 
+type ResolvedRequest = { data: unknown } & Request;
+
 export class DeferredApiHelper implements ApiHelper {
   private readonly _pendingRequests: PendingRequest[] = [];
-  readonly resolvedRequests: Request[] = [];
+  readonly resolvedRequests: ResolvedRequest[] = [];
 
   get pendingRequests(): Request[] {
     return this._pendingRequests.map((p) => p.request);
   }
 
+  private resolvePendingRequest(
+    { resolve, promise, request }: PendingRequest,
+    data: unknown
+  ): Promise<unknown> {
+    this.resolvedRequests.push({ ...request, data });
+    resolve(data);
+    return promise;
+  }
+
+  /**
+   * Resolves the first pending request
+   */
   resolve(data: unknown): Promise<unknown> {
     const pendingRequest = this._pendingRequests.shift();
     if (typeof pendingRequest === "undefined") {
       throw new Error("No available invocations");
     }
 
-    const { resolve, promise, request } = pendingRequest;
-    this.resolvedRequests.push(request);
-    resolve(data);
-    return promise;
+    return this.resolvePendingRequest(pendingRequest, data);
+  }
+
+  private extractPendingRequest(request: Request): PendingRequest | undefined {
+    return remove(this._pendingRequests, (req) =>
+      isEqual(req.request, request)
+    )[0];
+  }
+
+  /**
+   * Resolves the first pending request that matches the subject
+   */
+  resolveRequest(subject: Request, data: unknown): Promise<unknown> {
+    const pendingRequest = this.extractPendingRequest(subject);
+    if (typeof pendingRequest === "undefined") {
+      throw new Error("No matching request found");
+    }
+    return this.resolvePendingRequest(pendingRequest, data);
+  }
+
+  /**
+   * Removes first pending request
+   */
+  ignore(): void {
+    this._pendingRequests.shift();
+  }
+
+  /**
+   * Removes first pending request that matches the subject
+   */
+  ignoreRequest(subject: Request): void {
+    this.extractPendingRequest(subject);
   }
 
   delete(url: string, environment: string): Promise<Maybe.Type<string>> {
