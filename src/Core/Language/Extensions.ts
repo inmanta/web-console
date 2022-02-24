0dc749f01a743ee78a35556fc9ef4a7e27254a88
Stijn Coolen <stijn.coolen@inmanta.com>
2022-02-24 11:31:48 +0100

    Add docs

--- src/Core/Language/Extensions.ts
+++ src/Core/Language/Extensions.ts
@@ -1,5 +1,6 @@
 /**
  * Add json serialisation support for bigints
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
  *
  * @NOTE This module needs to be imported somewhere
  * for this extension to take effect.
