3e07eef2295e39fee52afbb1eabfa1b6ef42e433
Stijn Coolen <stijn.coolen@inmanta.com>
2022-03-02 16:57:08 +0100

    Use ResourceLink

--- src/UI/Pages/Resources/ResourceTableRow.tsx
+++ src/UI/Pages/Resources/ResourceTableRow.tsx
@@ -1,15 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Button } from "@patternfly/react-core";
 import { AngleDownIcon, AngleRightIcon } from "@patternfly/react-icons";
 import { Tbody, Tr, Td, ExpandableRowContent } from "@patternfly/react-table";
 import styled from "styled-components";
 import { Resource } from "@/Core";
 import {
-  Link,
   RequiresTableWithData,
+  ResourceLink,
   ResourceStatusLabel,
 } from "@/UI/Components";
-import { DependencyContext } from "@/UI/Dependency";
 import { words } from "@/UI/words";
 
 interface Props {
@@ -24,50 +23,44 @@ export const ResourceTableRow: React.FC<Props> = ({
   isExpanded,
   onToggle,
   numberOfColumns,
-}) => {
-  const { routeManager } = useContext(DependencyContext);
-  return (
-    <Tbody>
-      <Tr aria-label="Resource Table Row">
-        <Td dataLabel={words("resources.column.type")}>{row.type}</Td>
-        <Td dataLabel={words("resources.column.agent")}>{row.agent}</Td>
-        <Td dataLabel={words("resources.column.value")}>{row.value}</Td>
-        <Td dataLabel={words("resources.column.requires")}>
-          <Button
-            variant="link"
-            icon={isExpanded ? <AngleDownIcon /> : <AngleRightIcon />}
-            iconPosition="left"
-            isDisabled={row.numberOfDependencies <= 0}
-            onClick={onToggle}
-          >
-            {row.numberOfDependencies}
-          </Button>
+}) => (
+  <Tbody>
+    <Tr aria-label="Resource Table Row">
+      <Td dataLabel={words("resources.column.type")}>{row.type}</Td>
+      <Td dataLabel={words("resources.column.agent")}>{row.agent}</Td>
+      <Td dataLabel={words("resources.column.value")}>{row.value}</Td>
+      <Td dataLabel={words("resources.column.requires")}>
+        <Button
+          variant="link"
+          icon={isExpanded ? <AngleDownIcon /> : <AngleRightIcon />}
+          iconPosition="left"
+          isDisabled={row.numberOfDependencies <= 0}
+          onClick={onToggle}
+        >
+          {row.numberOfDependencies}
+        </Button>
+      </Td>
+      <Td dataLabel={words("resources.column.deployState")}>
+        <ResourceStatusLabel status={row.deployState} />
+      </Td>
+      <StyledCell>
+        <ResourceLink
+          resourceId={row.id}
+          linkText={words("resources.link.details")}
+        />
+      </StyledCell>
+    </Tr>
+    {isExpanded && (
+      <Tr isExpanded={isExpanded}>
+        <Td colSpan={numberOfColumns}>
+          <ExpandableRowContent>
+            <RequiresTableWithData id={row.id} />
+          </ExpandableRowContent>
         </Td>
-        <Td dataLabel={words("resources.column.deployState")}>
-          <ResourceStatusLabel status={row.deployState} />
-        </Td>
-        <StyledCell>
-          <Link
-            pathname={routeManager.getUrl("ResourceDetails", {
-              resourceId: row.id,
-            })}
-          >
-            <Button variant="link">{words("resources.link.details")}</Button>
-          </Link>
-        </StyledCell>
       </Tr>
-      {isExpanded && (
-        <Tr isExpanded={isExpanded}>
-          <Td colSpan={numberOfColumns}>
-            <ExpandableRowContent>
-              <RequiresTableWithData id={row.id} />
-            </ExpandableRowContent>
-          </Td>
-        </Tr>
-      )}
-    </Tbody>
-  );
-};
+    )}
+  </Tbody>
+);
 
 const StyledCell = styled(Td)`
   text-align: right;
