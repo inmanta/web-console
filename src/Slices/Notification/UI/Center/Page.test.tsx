ab276ebaf8a99c7570f37df64757fe4f249d3119
Stijn Coolen <stijn.coolen@inmanta.com>
2022-03-22 13:18:18 +0100

    Add test todos

---
+++ src/Slices/Notification/UI/Center/Page.test.tsx
@@ -0,0 +1,21 @@
+test.todo("Given Notification Center page Then fetches notifications");
+
+test.todo(
+  "Given Notification Center page When user filters on severity Then executes correct request"
+);
+
+test.todo(
+  "Given Notification Center page When user filters on title Then executes correct request"
+);
+
+test.todo(
+  "Given Notification Center page When user filters on message Then executes correct request"
+);
+
+test.todo(
+  "Given Notification Center page When user filters on read Then executes correct request"
+);
+
+test.todo(
+  "Given Notification Center page When user selects 2nd page Then fetches 2nd page"
+);
