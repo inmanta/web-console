45ba3548166aae2afd9a3850f43c92a2886ed0df
Sander Van Balen <git@sandervanbalen.be>
2022-03-23 15:02:37 +0100

    added conflicting files

---
+++ src/UI/Components/FilterPicker/FilterPicker.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import {
+  ToolbarItem,
+  Select,
+  SelectVariant,
+  SelectOption,
+} from "@patternfly/react-core";
+import { FilterIcon } from "@patternfly/react-icons";
+import styled from "styled-components";
+
+interface Props {
+  filterKind: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setFilterKind: (kind: any) => void;
+  items: string[];
+}
+
+export const FilterPicker: React.FC<Props> = ({
+  filterKind,
+  setFilterKind,
+  items,
+}) => {
+  const [isFilterOpen, setFilterOpen] = useState(false);
+
+  const onSelect = (event, selection) => {
+    setFilterOpen(false);
+    setFilterKind(selection);
+  };
+
+  return (
+    <StyledToolbarItem>
+      <Select
+        variant={SelectVariant.single}
+        toggleAriaLabel="FilterPicker"
+        onToggle={setFilterOpen}
+        onSelect={onSelect}
+        toggleIcon={<FilterIcon />}
+        selections={filterKind}
+        isOpen={isFilterOpen}
+      >
+        {items.map((item) => (
+          <SelectOption key={item} value={item}>
+            {item}
+          </SelectOption>
+        ))}
+      </Select>
+    </StyledToolbarItem>
+  );
+};
+
+const StyledToolbarItem = styled(ToolbarItem)`
+  align-self: start;
+`;
