5008e725799587b85929872e953f2ca12a492229
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-04-04 15:19:57 +0200

    Fix test case

--- src/UI/Pages/Resources/Page.test.tsx
+++ src/UI/Pages/Resources/Page.test.tsx
@@ -290,28 +290,6 @@ test("GIVEN ResourcesView WHEN data is loading for next page THEN shows toolbar"
   expect(
     screen.getByRole("generic", { name: "PaginationWidget" })
   ).toBeVisible();
-
-  apiHelper.resolve(
-    Either.right({
-      ...Resource.response,
-      metadata: {
-        ...Resource.response.metadata,
-        deploy_summary: {
-          ...Resource.response.metadata.deploy_summary,
-          by_state: {
-            ...Resource.response.metadata.deploy_summary.by_state,
-            available: 2,
-          },
-        },
-      },
-    })
-  );
-
-  expect(
-    await screen.findByRole("generic", {
-      name: "LegendItem-available",
-    })
-  ).toHaveAttribute("data-value", "2");
 });
 
 test("GIVEN ResourcesView WHEN data is auto-updated THEN shows updated toolbar", async () => {
@@ -375,26 +353,6 @@ test("GIVEN ResourcesView WHEN data is auto-updated THEN shows updated toolbar",
       name: "LegendItem-available",
     })
   ).toHaveAttribute("data-value", "2");
-});
-
-test("ResourcesView shows deploy state bar with available status without processing_events status", async () => {
-  const { component, apiHelper } = setup();
-  render(component);
-  apiHelper.resolve(Either.right(Resource.response));
-
-  expect(
-    await screen.findByRole("generic", { name: "Deployment state summary" })
-  ).toBeInTheDocument();
-
-  const availableItem = screen.getByRole("generic", {
-    name: "LegendItem-available",
-  });
-  expect(availableItem).toBeVisible();
-  expect(availableItem).toHaveAttribute("data-value", "1");
-  expect(availableItem).not.toHaveAttribute("data-value", "3");
-  expect(
-    screen.getByRole("cell", { name: "Status-processing_events" })
-  ).toBeVisible();
 });
 
 test("Given the ResourcesView When clicking on deploy, then the approriate backend request is fired", async () => {
