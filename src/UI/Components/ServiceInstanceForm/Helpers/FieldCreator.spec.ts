69ba3804f45fffcaee84af3f87ca039c56903e3e
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-04-04 09:16:15 +0200

    Mark fields with empty string default value as optional in the service instance forms (Issue #3211, PR #3232)
    # Description
    
    closes #3211
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [x] ~End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/UI/Components/ServiceInstanceForm/Helpers/FieldCreator.spec.ts
+++ src/UI/Components/ServiceInstanceForm/Helpers/FieldCreator.spec.ts
@@ -21,3 +21,47 @@ test("GIVEN FieldCreator for edit form WHEN create is provided with a service TH
   expect((fields[4] as NestedField).fields).toHaveLength(2);
   expect((fields[5] as NestedField).fields).toHaveLength(1);
 });
+
+test("GIVEN FieldCreator WHEN an attribute has the empty string as default value THEN the field is marked as optional", () => {
+  const stringAttribute = {
+    name: "attribute_with_empty_default_value",
+    description: "desciption",
+    modifier: "rw+",
+    type: "string",
+    default_value: "",
+    default_value_set: true,
+    validation_type: null,
+    validation_parameters: null,
+  };
+  const attributes = [
+    stringAttribute,
+    {
+      ...stringAttribute,
+      name: "attribute_with_no_default_value",
+      default_value: null,
+      default_value_set: false,
+    },
+    {
+      ...stringAttribute,
+      name: "attribute_with_explicit_default_value",
+      default_value: "default",
+      default_value_set: true,
+    },
+    {
+      ...stringAttribute,
+      name: "attribute_with_null_default_value",
+      default_value: null,
+      default_value_set: true,
+      type: "string?",
+    },
+  ];
+  const fields = new FieldCreator(new CreateModifierHandler()).create({
+    attributes: attributes,
+    embedded_entities: [],
+  });
+  expect(fields).toHaveLength(attributes.length);
+  expect(fields[0].isOptional).toBeTruthy();
+  expect(fields[1].isOptional).toBeFalsy();
+  expect(fields[2].isOptional).toBeFalsy();
+  expect(fields[3].isOptional).toBeTruthy();
+});
