a74da3b540be5abceca4600105188e597b4fbccc
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-04-04 09:07:59 +0200

    Simplify it more

--- src/UI/Root/Components/PageFrame/PageInitializer.tsx
+++ src/UI/Root/Components/PageFrame/PageInitializer.tsx
@@ -1,27 +1,17 @@
 import React, { useContext } from "react";
-import { RemoteData } from "@/Core";
 import { DependencyContext } from "@/UI/Dependency";
 
 interface Props {
   environment: string;
 }
 
-export const PageInitializer: React.FC<Props> = ({ children, environment }) => {
+export const PageInitializer: React.FC<Props> = ({ environment }) => {
   const { queryResolver } = useContext(DependencyContext);
 
-  const [environmentSettingsData] =
-    queryResolver.useOneTime<"GetEnvironmentSettings">({
-      kind: "GetEnvironmentSettings",
-      environment: environment,
-    });
+  const [] = queryResolver.useOneTime<"GetEnvironmentSettings">({
+    kind: "GetEnvironmentSettings",
+    environment: environment,
+  });
 
-  return RemoteData.fold(
-    {
-      notAsked: () => null,
-      loading: () => <>{children}</>,
-      failed: () => <>{children}</>,
-      success: () => <>{children}</>,
-    },
-    environmentSettingsData
-  );
+  return <></>;
 };
