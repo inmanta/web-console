ddcfa7d4d033d51a8a03fa5d7f4f641dae6188f5
Andras Kovari <55082127+andraskvr@users.noreply.github.com>
2022-04-07 11:26:09 +0200

    Test cases

--- src/UI/Components/DesiredStateAttributes/AttributeClassifier.spec.ts
+++ src/UI/Components/DesiredStateAttributes/AttributeClassifier.spec.ts
@@ -1,3 +1,4 @@
+import { Maybe } from "@/Core";
 import { JsonFormatter, XmlFormatter } from "@/Data";
 import { AttributeClassifier } from "./AttributeClassifier";
 import { attributes, classified } from "./Data";
@@ -9,4 +10,28 @@ test("GIVEN AttributeClassifier WHEN provided with a mixed attributes object THE
   );
 
   expect(classifier.classify(attributes)).toEqual(classified);
+});
+
+test("GIVEN AttributeClassifier WHEN provided with a custom multiline classifier THEN returns the correct list of ClassifiedAttributes", () => {
+  const classifier = new AttributeClassifier(
+    new JsonFormatter(),
+    new XmlFormatter(),
+    (key: string, value: string) => Maybe.some({ kind: "Python", key, value })
+  );
+
+  expect(
+    classifier.classify({ f: attributes["f"], ff: attributes["ff"] })
+  ).toEqual([
+    {
+      kind: "Python",
+      key: "f",
+      value:
+        "This text has a length longer than 80. abcdefghijklmnopqrstvuwxyz abcdefghijklmnopqrstvuwxyz",
+    },
+    {
+      kind: "Python",
+      key: "ff",
+      value: "This text contains a newline.\nblablabla...",
+    },
+  ]);
 });
