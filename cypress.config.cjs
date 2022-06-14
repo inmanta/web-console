39fc7e1c68af0097d0e256d0840917b1428b683a
Lejoly Florent <florent.lejoly@inmanta.com>
2022-06-14 10:43:11 +0200

    update

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,6 +1,7 @@
-import { defineConfig } from "cypress";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   video: false,
   reporter: "junit",
   reporterOptions: {
