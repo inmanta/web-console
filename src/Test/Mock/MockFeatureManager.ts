6c337d99ebe4e0fbcad0c0312bc7b55ca5723c6d
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-09-26 11:47:11 +0200

    Improve test coverage for conditionals (Issue #4000, PR #4091)
    # Description
    
    * Short description here *
    
    closes *Add ticket reference here*
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [x] Attached issue to pull request
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    ~~- [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )~~
    
    # Reviewer Checklist:
    
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Code is clear and sufficiently documented
    - [ ] Correct, in line with design

--- src/Test/Mock/MockFeatureManager.ts
+++ src/Test/Mock/MockFeatureManager.ts
@@ -29,3 +29,33 @@ export class MockFeatureManager implements FeatureManager {
     return "Standard Edition";
   }
 }
+
+export class MockEditableFeatureManager implements FeatureManager {
+  getCommitHash(): string {
+    return "123456abcdef";
+  }
+
+  getJsonParser(): JsonParserId {
+    return "Native";
+  }
+
+  getServerVersion(): string {
+    return "4.1";
+  }
+
+  isSupportEnabled(): boolean {
+    return true;
+  }
+
+  getServerMajorVersion(): string {
+    return "4";
+  }
+
+  isLsmEnabled(): boolean {
+    return true;
+  }
+
+  getEdition(): string {
+    return "Open Source";
+  }
+}
