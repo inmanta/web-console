bd4368574c032104705fe0acf447f6414ff34f86
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-10-21 15:12:04 +0200

    improve rwd of header

--- src/UI/Root/Components/Header/Header.tsx
+++ src/UI/Root/Components/Header/Header.tsx
@@ -34,4 +34,24 @@ const StyledImage = styled(SimpleBackgroundImage)`
 
 const StyledHeader = styled(PageHeader)`
   background-color: transparent;
+  //var(--pf-global--breakpoint--md) doesn't seems work
+  @media (min-width: 768px) {
+    grid-template-columns: auto 1fr auto;
+  }
+  .pf-c-page__header-brand {
+    @media (min-width: 768px) {
+      padding-right: 2rem;
+    }
+  }
+  .pf-c-page__header-nav {
+    @media (min-width: 768px) {
+      grid-column: 2/3;
+      grid-row: 1/2;
+    }
+  }
+  .pf-c-page__header-tools {
+    @media (min-width: 768px) {
+      grid-column: 3/3;
+    }
+  }
 `;
