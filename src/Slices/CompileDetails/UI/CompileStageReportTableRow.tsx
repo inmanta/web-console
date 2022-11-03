51a553a094c5c946460cd179461e2f38584a97e5
laura stordeur <stordeur.laura@outlook.fr>
2022-11-03 09:12:10 +0100

    hopefully fix typing issues now

--- src/Slices/CompileDetails/UI/CompileStageReportTableRow.tsx
+++ src/Slices/CompileDetails/UI/CompileStageReportTableRow.tsx
@@ -72,7 +72,7 @@ export const CompileStageReportTableRow: React.FC<Props> = ({
                 {words("compileDetails.stages.columns.returnCode")}
               </DescriptionListTerm>
               <DescriptionListDescription>
-                {row.returncode}
+                <>{row.returncode}</>
               </DescriptionListDescription>
             </DescriptionListGroup>
             <DescriptionListGroup>
