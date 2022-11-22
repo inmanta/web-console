e0b8cc1302da8c6e4ac1e3855210f54841c1dc51
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-11-22 12:07:28 +0100

    docstring

--- src/Core/Domain/Uuid.ts
+++ src/Core/Domain/Uuid.ts
@@ -8,4 +8,10 @@ export const getUuidFromRaw = (raw: string): Uuid => ({
   short: raw.substring(0, 4),
 });
 
+/**
+ * Function to get shorten version of uuid
+ *
+ * @param raw full length uuid string
+ * @returns first 8 characters of the uuid
+ */
 export const getShortUuidFromRaw = (raw: string): string => raw.substring(0, 8);
