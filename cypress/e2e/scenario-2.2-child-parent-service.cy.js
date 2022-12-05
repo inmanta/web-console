a579d73a5752df2078dddc0d1f85ad72d9a483c5
laura stordeur <stordeur.laura@outlook.fr>
2022-12-05 16:26:30 +0100

    stash test

---
+++ cypress/e2e/scenario-2.2-child-parent-service.cy.js
@@ -0,0 +1,78 @@
+/**
+ * Shorthand method to clear the environment being passed.
+ * By default, if no arguments are passed it will target the 'lsm-frontend' environment.
+ *
+ * @param {string} nameEnvironment
+ */
+const clearEnvironment = (nameEnvironment = "lsm-frontend") => {
+  cy.visit("/console/");
+  cy.get('[aria-label="Environment card"]').contains(nameEnvironment).click();
+  cy.url().then((url) => {
+    const location = new URL(url);
+    const id = location.searchParams.get("env");
+    cy.request("DELETE", `/api/v1/decommission/${id}`);
+  });
+};
+
+/**
+ * based on the environment id, it will recursively check if a compile is pending.
+ * It will continue the recursion as long as the statusCode is equal to 200
+ *
+ * @param {string} id
+ */
+const checkStatusCompile = (id) => {
+  let statusCodeCompile = 200;
+
+  if (statusCodeCompile === 200) {
+    cy.intercept(`/api/v1/notify/${id}`).as("IsCompiling");
+    // the timeout is necessary to avoid errors.
+    // Cypress doesn't support while loops and this was the only workaround to wait till the statuscode is not 200 anymore.
+    // the default timeout in cypress is 5000, but since we have recursion it goes into timeout for the nested awaits because of the recursion.
+    cy.wait("@IsCompiling", { timeout: 10000 }).then((req) => {
+      statusCodeCompile = req.response.statusCode;
+
+      if (statusCodeCompile === 200) {
+        checkStatusCompile(id);
+      }
+    });
+  }
+};
+
+/**
+ * when ID of the environment is not yet known, use this method to wait for the compile to be terminated.
+ */
+const waitForCompile = () => {
+  cy.url().then((url) => {
+    const location = new URL(url);
+    const id = location.searchParams.get("env");
+    checkStatusCompile(id);
+  });
+};
+
+/**
+ * Will by default execute the force update on the 'lsm-frontend' environment if no argumenst are being passed.
+ * This method can be executed standalone, but is part of the cleanup cycle that is needed before running a scenario.
+ *
+ * @param {string} nameEnvironment
+ */
+const forceUpdateEnvironment = (nameEnvironment = "lsm-frontend") => {
+  cy.visit("/console/");
+  cy.get('[aria-label="Environment card"]').contains(nameEnvironment).click();
+  cy.url().then((url) => {
+    const location = new URL(url);
+    const id = location.searchParams.get("env");
+    cy.request({
+      method: "POST",
+      url: `/lsm/v1/exporter/export_service_definition`,
+      headers: { "X-Inmanta-Tid": id },
+      body: { force_update: false },
+    });
+    checkStatusCompile(id);
+  });
+};
+
+before(() => {
+  clearEnvironment();
+  forceUpdateEnvironment();
+  waitForCompile();
+});
