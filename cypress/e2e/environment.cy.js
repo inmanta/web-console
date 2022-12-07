6b13a072b14a109d3cda6150bb5f020d90b48521
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-12-07 13:17:12 +0100

    fixes

--- cypress/e2e/environment.cy.js
+++ cypress/e2e/environment.cy.js
@@ -129,15 +129,15 @@ const testName = (number) => "TestName " + number;
 
 beforeEach(() => {
   // delete projects exlcuding test one before each test to have unified conditions for each test case
+  cy.intercept("/api/v2/environment").as("createEnv");
   cy.request("/api/v1/project").as("projects");
   cy.get("@projects").then((response) => {
     response.body.projects.map((project) => {
       if (project.name !== "lsm-frontend") {
-        cy.request("DELETE", `/api/v1/project/${project.id}`);
+        cy.request("DELETE", `api/v1/project/${project.id}`);
       }
     });
   });
-  cy.wait(600);
 });
 /**
  * Function is responsible for creating Environment process without submiting it.
@@ -146,18 +146,18 @@ beforeEach(() => {
  * @param {*} envName -string
  * @param {*} projectName -string
  * @param {*} shouldPassEnvName -boolean - default value - true
- * @param {*} envName -boolean - default value - false
+ * @param {*} fillOptionalInputs -boolean - default value - false
  */
-const createEnv = ({
+const fillCreateEnvForm = ({
   envName,
   projectName,
   shouldPassEnvName = true,
   fillOptionalInputs = false,
 }) => {
   cy.get('[aria-label="Project Name-typeahead"]').type(projectName);
-  cy.get('[aria-label="Project Name-select-input').within(() => {
-    cy.get(".pf-c-select__menu-item").click();
-  });
+  cy.get('[aria-label="Project Name-select-input')
+    .find(".pf-c-select__menu-item")
+    .click();
   if (shouldPassEnvName) {
     cy.get('[aria-label="Name-input"]').type(envName);
   }
@@ -205,7 +205,7 @@ describe("Environment", function () {
       "eq",
       Cypress.config().baseUrl + "/console/environment/create"
     );
-    createEnv({
+    fillCreateEnvForm({
       envName: testName(1),
       projectName: testProjectName(1),
     });
@@ -216,42 +216,42 @@ describe("Environment", function () {
   it("1.2 Create new  environment", function () {
     //fill the form and submit
     cy.visit("/console/environment/create");
-    createEnv({
+    fillCreateEnvForm({
       envName: testName(2),
       projectName: testProjectName(2),
       shouldPassEnvName: false,
     });
     cy.get("button").contains("Submit").should("be.disabled");
     cy.get('[aria-label="Name-input"]').type(testName(2));
-    cy.wait(600);
     cy.get("button").contains("Submit").should("not.be.disabled").click();
-    cy.wait(600);
+    cy.wait("@createEnv", { timeout: 10000 })
+      .its("response.statusCode")
+      .should("eq", 200);
     cy.url().should("contain", "/console/lsm/catalog?env=");
     //go back to gome and check if env is visible
-    cy.wait(50);
     cy.get(".pf-c-breadcrumb__item")
       .contains("Home")
       .should("be.visible")
       .click();
-    cy.url().should("eq", Cypress.config().baseUrl + "/console/");
     cy.get('[aria-label="Environment card"]').should(
       "any.contain",
       testName(2)
     );
   });
 
-  it("1.3 delete an environment", function () {
+  it("1.3 delete an environment", () => {
     //Fill The form and submit
     cy.visit("/console/environment/create");
-    createEnv({
+    fillCreateEnvForm({
       envName: testName(3),
       projectName: testProjectName(3),
       shouldPassEnvName: true,
       fillOptionalInputs: true,
     });
-    cy.wait(600);
     cy.get("button").contains("Submit").should("not.be.disabled").click();
-    cy.wait(600);
+    cy.wait("@createEnv", { timeout: 10000 })
+      .its("response.statusCode")
+      .should("eq", 200);
     cy.url().should("contain", "/console/lsm/catalog?env=");
 
     openSettings(testName(3));
@@ -261,80 +261,76 @@ describe("Environment", function () {
   it("1.4 Edit created environment", function () {
     //Fill The form and submit
     cy.visit("/console/environment/create");
-    createEnv({
+    fillCreateEnvForm({
       envName: testName(4),
       projectName: testProjectName(4),
       shouldPassEnvName: true,
       fillOptionalInputs: true,
     });
-    cy.wait(600);
     cy.get("button").contains("Submit").should("not.be.disabled").click();
-    cy.wait(600);
+    cy.wait("@createEnv").its("response.statusCode").should("eq", 200);
     cy.url().should("contain", "/console/lsm/catalog?env=");
 
     openSettings(testName(4));
     //change Name value
-    cy.wait(50);
     cy.get('[aria-label="Name-toggle-edit"]').click();
-    cy.get('[aria-label="Name-input"]').clear().type("New Value Name");
+    cy.get('[aria-label="Name-input"]').clear();
+    cy.get('[aria-label="Name-input"]').type("New Value Name");
     cy.get('[aria-label="Name-submit-edit"]').click();
     cy.get('[aria-label="Name-value"]').should("contain", "New Value Name");
     //change Description value
-    cy.wait(600);
     cy.get('[aria-label="Description-toggle-edit"]')
-      .should("be.visible")
+      .should(($el) => {
+        expect(Cypress.dom.isDetached($el)).to.eq(false);
+      })
       .click();
-    cy.wait(100);
-    cy.get('[aria-label="Description-input"]')
-      .should("be.visible")
-      .clear()
-      .type("New Value Description");
+    cy.get('[aria-label="Description-input"]').should("be.visible").clear();
+    cy.get('[aria-label="Description-input"]').type("New Value Description");
     cy.get('[aria-label="Description-submit-edit"]').click();
     cy.get('[aria-label="Description-value"]').should(
       "contain",
       "New Value Description"
     );
     //change Repository Branch value
-    cy.wait(500);
     cy.get('[aria-label="Repository Settings-toggle-edit"]')
       .should("be.visible")
       .click();
-    cy.wait(500);
-    cy.get('[aria-label="repo_branch-input"]')
-      .clear()
-      .type("New Value Repo Branch");
+    cy.get('[aria-label="repo_branch-input"]').clear();
+    cy.get('[aria-label="repo_branch-input"]').type("New Value Repo Branch", {
+      delay: 10,
+    });
     cy.get('[aria-label="Repository Settings-submit-edit"]').click();
     cy.get('[aria-label="repo_branch-value"]').should(
       "contain",
       "New Value Repo Branch"
     );
     //change Repository url value
-    cy.wait(600);
     cy.get('[aria-label="Repository Settings-toggle-edit"]')
       .should("be.visible")
       .click();
-    cy.wait(500);
 
-    cy.get('[aria-label="repo_url-input"]').clear().type("New Value Repo Url");
+    cy.get('[aria-label="repo_url-input"]').clear();
+    cy.get('[aria-label="repo_url-input"]').type("New Value Repo Url");
     cy.get('[aria-label="Repository Settings-submit-edit"]').click();
     cy.get('[aria-label="repo_url-value"]').should(
       "contain",
       "New Value Repo Url"
     );
     //change Project Name value
-    cy.wait(500);
     cy.get('[aria-label="Project Name-toggle-edit"]').click();
-    cy.wait(500);
-    cy.get('[aria-label="Project Name-typeahead"]')
-      .clear()
-      .type("New Value Project Name");
+    cy.get('[aria-label="Project Name-typeahead"]').clear();
+    cy.get('[aria-label="Project Name-typeahead"]').type(
+      "New Value Project Name"
+    );
 
     cy.get("button").contains('Create "New Value Project Name"').click();
     cy.get('[aria-label="Project Name-submit-edit"]').click({ force: true });
     //change Icon value
-    cy.wait(500);
-    cy.get('[aria-label="Icon-toggle-edit"]').should("be.visible").click();
-    cy.wait(500);
+    cy.get('[aria-label="Icon-toggle-edit"]')
+      .should(($el) => {
+        expect(Cypress.dom.isDetached($el)).to.eq(false);
+      })
+      .click();
     cy.get("#simple-text-file-filename").selectFile(icon, {
       action: "drag-drop",
       force: true,
@@ -387,94 +383,80 @@ describe("Environment", function () {
 
   it("1.6 Edit environment configuration", function () {
     cy.visit("/console/environment/create");
-    createEnv({
+    fillCreateEnvForm({
       envName: testName(6),
       projectName: testProjectName(6),
       shouldPassEnvName: true,
       fillOptionalInputs: true,
     });
-    cy.wait(600);
     cy.get("button").contains("Submit").should("not.be.disabled").click();
-    cy.wait(600);
+    cy.wait("@createEnv", { timeout: 10000 })
+      .its("response.statusCode")
+      .should("eq", 200);
     cy.url().should("contain", "/console/lsm/catalog?env=");
 
     openSettings(testName(6), testProjectName(6));
     cy.get("button").contains("Configuration").click();
     //go through every configuration and change its value according to one assigned in the configruationTypes
     cy.wrap(configurationTypes).each((config) => {
-      cy.wait(100);
       switch (config.inputType) {
         case "text":
-          cy.get(config.row).clear().type(config.newValue);
+          cy.get(config.row).clear();
+          cy.get(config.row).type(config.newValue);
           break;
         case "textNumber":
-          cy.get(config.row).within(() => {
-            cy.get(".pf-c-form-control").clear().type(config.newValue);
-          });
+          cy.get(config.row).find(".pf-c-form-control").clear();
+          cy.get(config.row).find(".pf-c-form-control").type(config.newValue);
           break;
         case "switch":
-          cy.get(config.row).within(() => {
-            cy.get(".pf-c-switch").click();
-          });
+          cy.get(config.row).find(".pf-c-switch").click();
 
           break;
         case "select":
-          cy.get(config.row).within(() => {
-            cy.get(".pf-c-form-control").click();
-          });
+          cy.get(config.row).find(".pf-c-form-control").click();
           cy.get(".pf-c-select__menu-item").contains(config.newValue).click();
 
           break;
         case "textmap":
-          cy.get(config.row).within(() => {
-            cy.get('[aria-label="editEntryValue"]')
-              .filter((key, $el) => {
-                return $el.value === "local:";
-              })
-              .clear()
-              .type(config.newValue);
-          });
+          cy.get(config.row)
+            .find('[aria-label="editEntryValue"]')
+            .filter((key, $el) => {
+              return $el.value === "local:";
+            })
+            .type("{selectAll}{backspace}" + config.newValue);
           break;
       }
       //assert if the warning sign exist, which means that value in the input is new but not saved, then save, and expect warning sign to disapear
-      cy.wait(100);
       cy.get('[aria-label="Warning"]').should("exist");
-      cy.get(config.row).within(() => {
-        cy.get('[aria-label="SaveAction"]').click();
-      });
-      cy.wait(100);
+      cy.get(config.row).find('[aria-label="SaveAction"]').click();
       cy.get('[aria-label="Warning"]').should("not.exist");
       //go through every configuration and expect new value there
       switch (config.inputType) {
         case "textNumber" | "text":
-          cy.get(config.row).within(() => {
-            cy.get(".pf-c-form-control").should("have.text", config.newValue);
-          });
+          cy.get(config.row)
+            .find(".pf-c-form-control")
+            .should("have.text", config.newValue);
           break;
         case "select":
-          cy.get(config.row).within(() => {
-            cy.get(".pf-c-form-control").should("have.value", config.newValue);
-          });
-
+          cy.get(config.row)
+            .find(".pf-c-form-control")
+            .should("have.value", config.newValue);
           break;
         case "textmap":
-          cy.get(config.row).within(() => {
-            cy.get('[aria-label="editEntryValue"]').should(
-              "have.value",
-              config.newValue
-            );
-          });
+          cy.get(config.row)
+            .find('[aria-label="editEntryValue"]')
+            .should("have.value", config.newValue);
           break;
       }
     });
     //re-enable to delete env
-    cy.get('[aria-label="Row-protected_environment"]').within(() => {
-      cy.get(".pf-c-switch").click();
-    });
+    cy.get('[aria-label="Row-protected_environment"]')
+      .find(".pf-c-switch")
+      .click();
     cy.get('[aria-label="Warning"]').should("exist");
-    cy.get('[aria-label="Row-protected_environment"]').within(() => {
-      cy.get('[aria-label="SaveAction"]').click();
-    });
+    cy.get('[aria-label="Row-protected_environment"]')
+      .find('[aria-label="SaveAction"]')
+      .click();
     cy.get('[aria-label="Warning"]').should("not.exist");
 
     cy.get("button").contains("Environment").click();
