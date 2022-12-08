e65ed0506deac5eb31ac39ee6fc5cd23ad36f8e5
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-12-08 10:20:55 +0100

    e2e

--- cypress/e2e/environment.cy.js
+++ cypress/e2e/environment.cy.js
@@ -338,7 +338,6 @@ describe("Environment", function () {
     cy.wait("@createProject");
     //change Icon value
     cy.get('[aria-label="Icon-toggle-edit"]').click();
-    cy.wait("@getEnv");
     cy.get("#simple-text-file-filename").selectFile(
       {
         contents: "@icon",
