9eac8d61441a387bff14657b7697600dd1895662
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-12-14 16:19:17 +0100

    change UX of booleans in instanceForm component

---
+++ src/UI/Components/ServiceInstanceForm/Components/BooleanToggleInput.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BooleanToggleInput } from "./BooleanToggleInput.tsx";
+
+describe("BooleanFormInput", () => {
+  const handleClick = jest.fn();
+  it("Should render toggle button input that reponds to the user input", async () => {
+    render(
+      <BooleanToggleInput
+        attributeName="bool_param"
+        isChecked={true}
+        isOptional={false}
+        description={"This is a bool parameter"}
+        handleInputChange={handleClick}
+      />
+    );
+    const toggle = await screen.findByLabelText("Toggle-bool_param");
+    await userEvent.click(toggle);
+    expect(handleClick).toHaveBeenCalled();
+  });
+});
