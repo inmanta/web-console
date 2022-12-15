d980fc6ddaa924860895ed2ee51d02889a1baa79
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2022-12-15 16:00:22 +0100

    remove backend trackers from e2e

--- cypress/e2e/scenario-2.3-embedded-entity.cy.js
+++ cypress/e2e/scenario-2.3-embedded-entity.cy.js
@@ -4,7 +4,7 @@
  *
  * @param {string} nameEnvironment
  */
-export const clearEnvironment = (nameEnvironment = "lsm-frontend") => {
+const clearEnvironment = (nameEnvironment = "lsm-frontend") => {
   cy.visit("/console/");
   cy.get('[aria-label="Environment card"]').contains(nameEnvironment).click();
   cy.url().then((url) => {
@@ -28,8 +28,9 @@ const checkStatusCompile = (id) => {
     // the timeout is necessary to avoid errors.
     // Cypress doesn't support while loops and this was the only workaround to wait till the statuscode is not 200 anymore.
     // the default timeout in cypress is 5000, but since we have recursion it goes into timeout for the nested awaits because of the recursion.
-    cy.wait("@IsCompiling").then((req) => {
+    cy.wait("@IsCompiling", { timeout: 15000 }).then((req) => {
       statusCodeCompile = req.response.statusCode;
+
       if (statusCodeCompile === 200) {
         checkStatusCompile(id);
       }
@@ -38,40 +39,12 @@ const checkStatusCompile = (id) => {
 };
 
 /**
- * It will recursively check for instance service status until its "up".
- * it has optional parameter to avoid infinite awaiting, with default value of 20
- * which is around 8 more than needed locally to fucntion to pass, but I took jenkins into account.
- *
- */
-const checkServiceState = (attempts = 20) => {
-  if (attempts < 1) {
-    return;
-  }
-  const updatedAttempts = attempts - 1;
-
-  cy.wait("@GetServiceInventory").then((req) => {
-    if (
-      req.response.statusCode !== 200 &&
-      req.response.statusCode !== 400 &&
-      req.response.statusCode !== 404 &&
-      req.response.statusCode !== 500
-    ) {
-      checkServiceState(updatedAttempts);
-    } else {
-      if (req.response.body.data[0].state !== "up") {
-        checkServiceState(updatedAttempts);
-      }
-    }
-  });
-};
-
-/**
  * Will by default execute the force update on the 'lsm-frontend' environment if no argumenst are being passed.
  * This method can be executed standalone, but is part of the cleanup cycle that is needed before running a scenario.
  *
  * @param {string} nameEnvironment
  */
-export const forceUpdateEnvironment = (nameEnvironment = "lsm-frontend") => {
+const forceUpdateEnvironment = (nameEnvironment = "lsm-frontend") => {
   cy.visit("/console/");
   cy.get('[aria-label="Environment card"]').contains(nameEnvironment).click();
   cy.url().then((url) => {
@@ -108,16 +81,10 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
     // Go from Home page to Service Inventory of embedded-service
     cy.visit("/console/");
 
-    cy.intercept(
-      "GET",
-      "/lsm/v1/service_inventory/embedded-entity-service?include_deployment_progress=True&limit=20&&sort=created_at.desc"
-    ).as("GetServiceInventory");
-
     cy.get('[aria-label="Environment card"]').contains("lsm-frontend").click();
     cy.get("#embedded-entity-service").contains("Show inventory").click();
 
     // make sure the call to get inventory has been executed
-    cy.wait("@GetServiceInventory");
     cy.get('[aria-label="ServiceInventory-Empty"]').should("to.be.visible");
 
     // Add an instance and fill form
@@ -146,10 +113,6 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
     cy.get("#address").type("1.2.3.7/32");
     cy.get("#vlan_id").type("3");
     cy.get("button").contains("Confirm").click();
-
-    // make sure the call to get inventory has been executed
-    cy.wait("@GetServiceInventory");
-
     // check if the view is still empty, also means we have been redirected as expected.
     cy.get('[aria-label="ServiceInventory-Success"]').should("to.be.visible");
   });
@@ -182,10 +145,6 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
   });
 
   it("2.3.3 - Show history view", () => {
-    cy.intercept(
-      "GET",
-      "lsm/v1/service_inventory/embedded-entity-service?include_deployment_progress=True&limit=20&&sort=created_at.desc"
-    ).as("GetServiceInventory");
     cy.visit("/console/");
     cy.get('[aria-label="Environment card"]').contains("lsm-frontend").click();
     // Expect to find one badge on the embedded-service row.
@@ -202,13 +161,14 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
     // Expect to find status tab
     cy.get(".pf-c-tabs__list li:first").should("have.class", "pf-m-current");
     //await for instance state to change to up
-    checkServiceState();
+    cy.get('[data-label="State"]')
+      .find(".pf-c-label.pf-m-green", { timeout: 60000 })
+      .should("contain", "up");
 
     cy.get(".pf-c-description-list").contains("History").click();
 
     // History sub-page should open and be empty then go to Home page
     cy.get("h1").contains("Service Instance History").should("be.visible");
-
     // due to lack of Id in rows I had to assert that each toggle button is separeate history log
     cy.get(".pf-c-table").find(".pf-c-table__toggle").should("have.length", 3);
     cy.visit("/console/");
@@ -217,29 +177,14 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
   it("2.1.4 Delete previously created instance", () => {
     cy.visit("/console/");
 
-    // Add interceptios for the delete and get call to be able to catch responses later on.
-    cy.intercept(
-      "DELETE",
-      "/lsm/v1/service_inventory/embedded-entity-service/**"
-    ).as("DeleteInstance");
-    cy.intercept(
-      "GET",
-      "/lsm/v1/service_inventory/embedded-entity-service?include_deployment_progress=True&limit=20&&sort=created_at.desc"
-    ).as("GetServiceInventory");
-
     cy.get('[aria-label="Environment card"]').contains("lsm-frontend").click();
     cy.get("#embedded-entity-service").contains("Show inventory").click();
 
-    // wait to check if compile is done
-    waitForCompile();
-    cy.wait("@GetServiceInventory");
-
     // expand first row
-    cy.get("#expand-toggle0").click();
+    cy.get("#expand-toggle0", { timeout: 20000 }).click();
 
     // There might be still a compile running because of the edit action. So we do a check if one is pending.
     waitForCompile();
-    cy.wait(2000);
 
     // delete but cancel deletion in modal
     cy.get(".pf-c-description-list").contains("Delete").click();
@@ -251,6 +196,8 @@ describe("Scenario 2.3 Service Catalog - embedded-entity", () => {
     cy.get(".pf-c-form__actions").contains("Yes").click();
 
     // check response if instance has been deleted succesfully.
-    cy.wait("@DeleteInstance").its("response.statusCode").should("eq", 200);
+    cy.get('[aria-label="ServiceInventory-Empty"]', { timeout: 40000 }).should(
+      "be.visible"
+    );
   });
 });
