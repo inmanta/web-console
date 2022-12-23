ad7d443a2839f727e540faef45ebf50fbd81e6ac
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2022-12-23 09:30:24 +0100

    Migrate the script that cleans up old npm packages from the graphql API to the rest API of Github. The graphql API is deprecated for packages (See: https://github.blog/changelog/2022-08-18-deprecation-notice-graphql-for-packages/). (PR #4475)
    # Description
    
    **Same PR as #4472, but applied on the iso4 branch only due to a merge conflict.**
    
    Migrate the script that cleans up old npm packages from the graphql API to the rest API of Github. The graphql API is deprecated for packages (See: https://github.blog/changelog/2022-08-18-deprecation-notice-graphql-for-packages/).
    
    # Self Check:
    
    - [ ] ~~Attached issue to pull request~~
    - [x] Changelog entry
    - [x] Code is clear and sufficiently documented
    - [x] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [x] Correct, in line with design
    - [ ] ~~End user documentation is included or an issue is created for end-user documentation~~

--- clean_up_packages.js
+++ clean_up_packages.js
@@ -1,111 +1,142 @@
 import fetch from "node-fetch";
+function getLinkToNextPage(headerObj) {
+  /**
+   *  Takes the Header object and returns the URL to the next page or null if no such page exists.
+   *
+   *  Format of link_header: '<URL>; rel="next", <URL>; rel="last"' where URL is a URL to a certain page.
+   */
+  if (!headerObj.has("link")) {
+    return null;
+  }
+  const linkHeader = headerObj.get("link");
+  const splittedLinkHeader = linkHeader.trim().split(",");
+  for (let i = 0; i < splittedLinkHeader.length; i++) {
+    const splittedHeaderParts = splittedLinkHeader[i].trim().split(";");
+    const refName = splittedHeaderParts[1].trim();
+    const refLink = splittedHeaderParts[0].trim();
+    if (refName === 'rel="next"') {
+      // The link is represented as '<url>'. This chops off the '<' and '>' characters.
+      return refLink.slice(1, refLink.length - 1);
+    }
+  }
+  return null;
+}
 
-// Get a list of packageVersionIds
-async function getDevVersions() {
-  const getQueryString = (cursor) =>
-    `{repository(owner:"inmanta",name:"web-console"){packages(first:1, names:"web-console"){nodes{packageType,name,id,versions(last:50, ${cursor} orderBy:{field:CREATED_AT, direction:DESC}){nodes{id,version,preRelease,files(first:1){nodes{name, updatedAt}}}, pageInfo{hasNextPage, hasPreviousPage,startCursor}}}}}}`;
-  let queryString = getQueryString("");
-  let moreRows = true;
-  while (moreRows) {
+async function getOldDevVersions() {
+  /**
+   *  Return a list of development packages that is older than 30 days.
+   *  The list of packages return by this method is not necessarily complete.
+   *  This is intentional, because we don't want to execute too many requests at once.
+   */
+  let queryUrl =
+    "https://api.github.com/orgs/inmanta/packages/npm/web-console/versions?per_page=100";
+  let result = [];
+  while (true) {
+    // This API endpoint sorts the packages from new to old.
     const queryResults = await fetch(
-      "https://api.github.com/graphql",
+      queryUrl,
       {
-        headers: { Authorization: `bearer ${process.env.GITHUB_TOKEN}` },
-        method: "POST",
-        body: JSON.stringify({
-          query: queryString,
-        }),
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          Accept: "application/vnd.github+json",
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+        method: "GET",
       },
       (error) => console.log(error)
-    ).then((result) => {
-      return result.json();
-    });
-    // Make sure we're working with the web-console
-    const webConsole = queryResults.data.repository.packages.nodes.find(
-      (pkg) => pkg.name === "web-console"
     );
+    const packages = await queryResults.json();
+    const response_headers = queryResults.headers;
 
-    if (!webConsole) {
+    if (!packages || !packages.length) {
       throw Error("Unable to find web-console packages");
     }
 
     // Take the dev versions only
-    const devVersions = webConsole.versions.nodes.filter((packageVersion) =>
-      packageVersion.version.includes("dev")
+    const devVersions = packages.filter((pkg) => pkg.name.includes("dev"));
+    const devVersionsWithDate = devVersions.map((devPackageVersion) => {
+      return {
+        id: devPackageVersion.id,
+        version: devPackageVersion.name,
+        updatedAt: devPackageVersion.updated_at,
+      };
+    });
+    // Take the versions that are older than 30 days
+    const oldDevVersions = devVersionsWithDate.filter(
+      (devPackageVersion) =>
+        Math.floor(
+          (new Date() - new Date(devPackageVersion.updatedAt)) /
+            (1000 * 60 * 60 * 24)
+        ) > 30
     );
 
+    if (result.length > 0 && oldDevVersions.length === 0) {
+      // We probably cleaned up until here during the previous run of this script.
+      // Don't go further back in history to limit the amount of requests.
+      return result;
+    }
+    result = result.concat(oldDevVersions);
+    if (result.length > 5) {
+      // Return as soon as we have at least five packages to not execute too many requests at once.
+      return result;
+    }
+
     // Check to see if another page should be queried
-    if (
-      devVersions.length == 0 &&
-      webConsole.versions.pageInfo.hasPreviousPage &&
-      webConsole.versions.pageInfo.startCursor
-    ) {
-      queryString = getQueryString(
-        `before: "${webConsole.versions.pageInfo.startCursor}",`
-      );
-      console.log(
-        `No dev versions found on this page of the results, fetching the previous page from ${webConsole.versions.pageInfo.startCursor}`
-      );
+    const linkToNextPage = getLinkToNextPage(response_headers);
+    if (linkToNextPage != null) {
+      queryUrl = linkToNextPage;
+      console.log(`Fetching the next page from ${queryUrl}`);
     } else {
-      console.log("Dev versions found, no need to query more pages");
-      moreRows = false;
-      return devVersions;
+      if (result.length === 0) {
+        console.log("No dev version exist");
+      }
+      return result;
     }
   }
 }
 
-getDevVersions().then((devVersions) => {
-  const devVersionsWithDate = devVersions
-    .map((devPackageVersion) => {
-      return {
-        id: devPackageVersion.id,
-        version: devPackageVersion.version,
-        updatedAt: devPackageVersion.files.nodes[0].updatedAt,
-      };
-    })
-    .reverse();
-  // Take the versions that are older than 30 days
-  let oldDevVersions = devVersionsWithDate.filter(
-    (devPackageVersion) =>
-      Math.floor(
-        (new Date() - new Date(devPackageVersion.updatedAt)) /
-          (1000 * 60 * 60 * 24)
-      ) > 30
-  );
-  if (oldDevVersions.length == 0) {
-    console.log("No old versions found matching the criteria");
-    return;
-  }
-  // Make sure we're not starting too many requests at once
-  if (oldDevVersions.length > 5) {
-    oldDevVersions = oldDevVersions.slice(0, 5);
-  }
-  oldDevVersions.map((oldDevVersion) => {
-    const idToDelete = oldDevVersion.id;
-    console.log(
-      `Attempting to delete version ${oldDevVersion.version} with id ${idToDelete} from ${oldDevVersion.updatedAt}`
-    );
-    fetch("https://api.github.com/graphql", {
-      headers: {
-        Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-        Accept: "application/vnd.github.package-deletes-preview+json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        query: `mutation { deletePackageVersion(input:{packageVersionId:"${idToDelete}"}) { success }}`,
-      }),
-    })
-      .then((result) => result.json())
-      .then((result) => {
-        if (result.errors && result.errors.length > 0) {
-          console.log(`Deleting package ${idToDelete} failed:`, result.errors);
-          throw Error(
-            `Deleting version ${oldDevVersion.id} failed:`,
-            JSON.stringify(result.errors)
-          );
-        } else {
-          console.log(`Successfully deleted package ${idToDelete}`);
+getOldDevVersions().then(
+  (oldDevVersions) => {
+    if (oldDevVersions.length == 0) {
+      console.log("No old versions found matching the criteria");
+      return;
+    }
+    // Make sure we're not starting too many requests at once
+    if (oldDevVersions.length > 5) {
+      oldDevVersions = oldDevVersions.slice(0, 5);
+    }
+    oldDevVersions.map((oldDevVersion) => {
+      const idToDelete = oldDevVersion.id;
+      console.log(
+        `Attempting to delete version ${oldDevVersion.version} with id ${idToDelete} from ${oldDevVersion.updatedAt}`
+      );
+      fetch(
+        `https://api.github.com/orgs/inmanta/packages/npm/web-console/versions/${idToDelete}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+            Accept: "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+          method: "DELETE",
         }
-      });
-  });
-});
+      ).then(
+        (response) => {
+          if (response.ok) {
+            console.log(`Successfully deleted package ${idToDelete}`);
+          } else {
+            const error_message = `Deleting package ${idToDelete} failed: ${response.statusText}`;
+            console.log(error_message);
+            throw Error(error_message);
+          }
+        },
+        (reason) => {
+          console.log(reason);
+        }
+      );
+    });
+  },
+  (reason) => {
+    console.log(`Failed to retrieve dev packages: ${reason}`);
+  }
+);
