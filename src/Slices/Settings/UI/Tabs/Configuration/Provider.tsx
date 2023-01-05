c58b8da6e5adaadd9d6134bbec7729233f20b0e3
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2023-01-05 09:44:39 +0100

    first itteration

--- src/Slices/Settings/UI/Tabs/Configuration/Provider.tsx
+++ src/Slices/Settings/UI/Tabs/Configuration/Provider.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import _ from "lodash";
 import { EnvironmentSettings } from "@/Core";
 import { DependencyContext } from "@/UI/Dependency";
 import { Container } from "./Container";
@@ -8,10 +9,42 @@ interface Props {
   settings: EnvironmentSettings.EnvironmentSettings;
 }
 
+function reducer(
+  state: { settings: EnvironmentSettings.ValuesMap; resetedValueName: string },
+  action: {
+    type: string;
+    payload: EnvironmentSettings.ValuesMap | string;
+  }
+) {
+  const { type, payload } = action;
+  switch (type) {
+    case "reset":
+      return {
+        settings: state.settings,
+        resetedValueName: payload as string,
+      };
+    case "update":
+      const copy = JSON.parse(JSON.stringify(state.settings));
+      if (state.resetedValueName !== "") {
+        delete copy[state.resetedValueName];
+      }
+      return {
+        settings: _.merge({}, JSON.parse(JSON.stringify(payload)), copy),
+        resetedValueName: "",
+      };
+    case "set":
+      return { ...state, settings: payload };
+    default:
+      return state;
+  }
+}
 export const Provider: React.FC<Props> = ({
   settings: { settings, definition },
 }) => {
-  const [values, setValues] = useState(settings);
+  const [state, dispatch] = useReducer(reducer, {
+    settings: settings,
+    resetedValueName: "",
+  });
   const [errorMessage, setErrorMessage] = useState("");
   const { commandResolver } = useContext(DependencyContext);
   const updateSetting =
@@ -23,17 +56,22 @@ export const Provider: React.FC<Props> = ({
       kind: "ResetEnvironmentSetting",
     }
   );
+  const handleReset = (id: string) => {
+    dispatch({ type: "reset", payload: id });
+    return resetSetting(id);
+  };
   const infos = new InputInfoCreator(
-    setValues,
+    (values: EnvironmentSettings.ValuesMap) => {
+      dispatch({ type: "set", payload: values });
+    },
     updateSetting,
-    resetSetting,
+    handleReset,
     setErrorMessage
-  ).create(settings, definition, values);
+  ).create(settings, definition, state.settings);
 
   useEffect(() => {
-    setValues(settings);
+    dispatch({ type: "update", payload: settings });
   }, [settings]);
-
   return (
     <Container
       infos={infos}
