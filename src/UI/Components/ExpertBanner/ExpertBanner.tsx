17dba2935d31fde9cd1b69703f81494bf743b9ee
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2023-03-08 15:09:39 +0100

    expert mode

---
+++ src/UI/Components/ExpertBanner/ExpertBanner.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { Banner } from "@patternfly/react-core";
+
+interface CustomEvent extends Event {
+  detail?: boolean;
+}
+export const ExpertBanner = () => {
+  const [showBannerState, setShowBannerState] = useState(false);
+  useEffect(() => {
+    document.addEventListener("expert-mode-check", (evt: CustomEvent) => {
+      setShowBannerState(evt.detail ? true : false);
+    });
+    return () =>
+      document.removeEventListener("expert-mode-check", (evt: CustomEvent) => {
+        setShowBannerState(evt.detail ? true : false);
+      });
+  }, []);
+  const banner = (
+    <React.Fragment>
+      <Banner isSticky variant="danger">
+        LSM expert mode is enabled, proceed with caution.
+      </Banner>
+    </React.Fragment>
+  );
+
+  return showBannerState ? banner : null;
+};
