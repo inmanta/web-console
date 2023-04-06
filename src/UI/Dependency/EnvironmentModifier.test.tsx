38467917035f98f0c563e1ac1e4f7b90bcadacac
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2023-04-06 11:04:26 +0200

    fix issue that missing setting and it's definition crash the app

--- src/UI/Dependency/EnvironmentModifier.test.tsx
+++ src/UI/Dependency/EnvironmentModifier.test.tsx
@@ -3,8 +3,10 @@ import { render, screen } from "@testing-library/react";
 import { StoreProvider } from "easy-peasy";
 import { act } from "react-dom/test-utils";
 import { EnvironmentModifier, RemoteData } from "@/Core";
+import { DefinitionMap } from "@/Core/Domain/EnvironmentSettings";
 import { getStoreInstance } from "@/Data";
 import { EnvironmentDetails, EnvironmentSettings } from "@/Test";
+import ErrorBoundary from "../Utils/ErrorBoundary";
 import { EnvironmentModifierImpl } from "./EnvironmentModifier";
 
 const DummyComponent: React.FC<{
@@ -17,28 +19,32 @@ const DummyComponent: React.FC<{
   );
 };
 
-function setup() {
+function setup(definition: DefinitionMap) {
   const environmentId = "env";
   const store = getStoreInstance();
   store.dispatch.environment.setSettingsData({
     environment: environmentId,
     value: RemoteData.success({
       settings: {},
-      definition: EnvironmentSettings.definition,
+      definition,
     }),
   });
   const environmentModifier = EnvironmentModifierImpl();
   environmentModifier.setEnvironment(environmentId);
   const component = (
-    <StoreProvider store={store}>
-      <DummyComponent environmentModifier={environmentModifier} />
-    </StoreProvider>
+    <ErrorBoundary>
+      <StoreProvider store={store}>
+        <DummyComponent environmentModifier={environmentModifier} />
+      </StoreProvider>
+    </ErrorBoundary>
   );
   return { component, store, environmentId };
 }
 
 test("Given the environmentModifier When the server compile setting is requested Then returns the correct value", async () => {
-  const { component, store, environmentId } = setup();
+  const { component, store, environmentId } = setup(
+    EnvironmentSettings.definition
+  );
   // No setting is specified, and the default is true
   store.dispatch.environment.setEnvironmentDetailsById({
     id: environmentId,
@@ -77,5 +83,23 @@ test("Given the environmentModifier When the server compile setting is requested
   });
   expect(
     await screen.findByRole("generic", { name: "server-compile-enabled" })
+  ).toBeVisible();
+});
+
+test("Given the environmentModifier When the missing setting is requested Then returns false without crashing the component", async () => {
+  delete EnvironmentSettings.definition.server_compile;
+  const { component, store, environmentId } = setup(
+    EnvironmentSettings.definition
+  );
+  // No setting is specified, and the dafault is missing
+  store.dispatch.environment.setEnvironmentDetailsById({
+    id: environmentId,
+    value: RemoteData.success({ ...EnvironmentDetails.a, settings: {} }),
+  });
+
+  render(component);
+  //expect to see div as the value would be false insteand of error boundary page
+  expect(
+    await screen.findByRole("generic", { name: "server-compile-disabled" })
   ).toBeVisible();
 });
