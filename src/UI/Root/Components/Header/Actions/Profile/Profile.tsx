ab5036dce0ba45fb295529ded3591651d7a277ce
laura stordeur <stordeur.laura@outlook.fr>
2023-06-12 12:14:40 +0200

    UI-fix for the header when authentication is enabled. (Issue #4865, PR #4891)
    # Description
    
    Solves [#4865](https://github.com/inmanta/web-console/issues/4865)
    #4871
    
    This PR only resolves a part of the keycloak issues. The dropdown is now displayed neatly. There are still issues with the state management and the caching. Also the logout action doesn't work yet, but this will be tackled in the next ticket.
    
    https://github.com/inmanta/web-console/assets/44098050/5f8bb434-4880-4578-a5ce-2dadef76a407

--- src/UI/Root/Components/Header/Actions/Profile/Profile.tsx
+++ src/UI/Root/Components/Header/Actions/Profile/Profile.tsx
@@ -1,44 +1,22 @@
 import React from "react";
-import {
-  Avatar,
-  DropdownItem,
-  PageHeaderToolsGroup,
-  TextContent,
-} from "@patternfly/react-core";
-import { AngleDownIcon } from "@patternfly/react-icons";
+import { TextContent } from "@patternfly/react-core";
 import Keycloak from "keycloak-js";
-import { IconDropdown } from "./IconDropdown";
-/* eslint-disable-next-line import/no-unresolved */
-import AvatarImg from "!url-loader!@assets/images/img_avatar.svg";
+import styled from "styled-components";
 
 interface Props {
   keycloak: Keycloak | undefined;
 }
 
-export const Profile: React.FC<Props> = ({ keycloak }) => (
-  <PageHeaderToolsGroup>
-    <Login keycloak={keycloak} />
-    <IconDropdown
-      icon={<AngleDownIcon />}
-      dropdownItems={[
-        <DropdownItem
-          key="action2"
-          component="button"
-          onClick={keycloak && (() => keycloak.logout())}
-        >
-          Logout
-        </DropdownItem>,
-      ]}
-    />
-    <Avatar src={AvatarImg} alt="Avatar image" />
-  </PageHeaderToolsGroup>
-);
-
-const Login: React.FC<Props> = ({ keycloak }) => {
-  const [name, setName] = React.useState("inmanta2");
+export const Profile: React.FC<Props> = ({ keycloak }) => {
+  const [name, setName] = React.useState("unknown user");
   if (keycloak && keycloak.profile && keycloak.profile.username !== name) {
     setName(keycloak.profile.username as string);
   }
 
-  return <TextContent>{name}</TextContent>;
+  return <StyledText>{name}</StyledText>;
 };
+
+const StyledText = styled(TextContent)`
+  font-weight: bold;
+  text-align: start;
+`;
