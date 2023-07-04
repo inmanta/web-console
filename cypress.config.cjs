64ef8de0c4977755381e7a9d4097150e32a82703
laura stordeur <stordeur.laura@outlook.fr>
2023-07-04 09:24:20 +0200

    remove fields from cypress config

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   env: {
     edition: "iso",
     project: "lsm-frontend",
-    keycloak: "true",
   },
   video: false,
   reporter: "junit",
@@ -15,7 +14,7 @@ module.exports = defineConfig({
     mochaFile: "cypress/reports/junit/test-report-[hash].xml",
   },
   e2e: {
-    baseUrl: "https://127.0.0.1:8888",
+    baseUrl: "http://127.0.0.1:8888",
     supportFile: false,
     defaultCommandTimeout: 10000,
     requestTimeout: 10000,
