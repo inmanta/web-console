9ade49929f23b81863f80a10cf2a6a09c08620aa
laura stordeur <stordeur.laura@outlook.fr>
2023-07-12 14:21:54 +0200

    update tests and method to truncate uri from ressourceVersionId

--- src/Test/Data/InstanceResource.ts
+++ src/Test/Data/InstanceResource.ts
@@ -1,22 +1,22 @@
 import { InstanceResourceModel, Resource } from "@/Core";
 
 export const a: InstanceResourceModel = {
-  resource_id: "resource_id_a,v=1",
+  resource_id: "[resource_id_a],v=1",
   resource_state: Resource.Status.deployed,
 };
 
 export const b: InstanceResourceModel = {
-  resource_id: "resource_id_b,v=2",
+  resource_id: "[resource_id_b],v=2",
   resource_state: Resource.Status.failed,
 };
 
 export const c: InstanceResourceModel = {
-  resource_id: "resource_id_c,v=2",
+  resource_id: "[resource_id_c],v=2",
   resource_state: Resource.Status.skipped,
 };
 
 export const d: InstanceResourceModel = {
-  resource_id: "resource_id_d,v=3",
+  resource_id: "[resource_id_d],v=3",
   resource_state: Resource.Status.deploying,
 };
 
