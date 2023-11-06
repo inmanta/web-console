3728f0e048a383712e984965fd9b8c2a913e99be
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2023-11-06 10:51:07 +0100

    remove jszip and blob generating functionality

--- src/Data/Common/PrimaryArchiveHelper.ts
+++ src/Data/Common/PrimaryArchiveHelper.ts
@@ -1,4 +1,3 @@
-import JSZip from "jszip";
 import { ArchiveHelper } from "@/Core";
 
 interface FileManager {
@@ -7,11 +6,6 @@ interface FileManager {
 
 export class PrimaryArchiveHelper implements ArchiveHelper {
   constructor(private readonly fileManager: FileManager) {}
-
-  async generateBlob(value: string): Promise<Blob> {
-    const zip = await JSZip.loadAsync(value, { base64: true });
-    return await zip.generateAsync({ type: "blob" });
-  }
 
   triggerDownload(blob: Blob): void {
     this.fileManager.download(blob, "support-archive.zip");
