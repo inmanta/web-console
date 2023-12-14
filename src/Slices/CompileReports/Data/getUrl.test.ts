11810c0c86c2d5d770632e61501374155435564a
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2023-12-14 23:48:45 +0800

    fix

--- src/Slices/CompileReports/Data/getUrl.test.ts
+++ src/Slices/CompileReports/Data/getUrl.test.ts
@@ -12,7 +12,7 @@ test.each`
   ${{ status: CompileStatus.inprogress }} | ${{ name: "requested", order: "desc" }} | ${"requested.desc"} | ${"20"}  | ${""}                                                       | ${`/api/v2/compilereport?limit=20&sort=requested.desc&filter.started=true&filter.completed=false`}
   ${{ status: CompileStatus.queued }}     | ${{ name: "requested", order: "desc" }} | ${"requested.desc"} | ${"20"}  | ${""}                                                       | ${`/api/v2/compilereport?limit=20&sort=requested.desc&filter.started=false`}
   ${{ status: CompileStatus.queued }}     | ${{ name: "requested", order: "desc" }} | ${"requested.desc"} | ${"20"}  | ${"end%3D2023-12-13T08%253A33%253A15.192674%252B00%253A00"} | ${`/api/v2/compilereport?limit=20&sort=requested.desc&filter.started=false&end%3D2023-12-13T08%253A33%253A15.192674%252B00%253A00`}
-  ${{ status: CompileStatus.queued }}     | ${{ name: "requested", order: "desc" }} | ${"requested.desc"} | ${"20"}  | ${"start=2023-12-13T08%3A33%3A15.180818%2B00%3A00"}         | ${`/api/v2/compilereport?limit=20&sort=requested.desc&filter.started=false&"start=2023-12-13T08%3A33%3A15.180818%2B00%3A00"`}
+  ${{ status: CompileStatus.queued }}     | ${{ name: "requested", order: "desc" }} | ${"requested.desc"} | ${"20"}  | ${"start=2023-12-13T08%3A33%3A15.180818%2B00%3A00"}         | ${`/api/v2/compilereport?limit=20&sort=requested.desc&filter.started=false&"start=2023-12-13T08%3A33%3A15.180818%2B00%3A00`}
 `(
   "getUrl returns correct url for compile reports with sort: $sortTxt, filter $filter, currentPage: $currentPage: and pageSize: $pageSize",
   ({ filter, sort, pageSize, url, currentPage }) => {
