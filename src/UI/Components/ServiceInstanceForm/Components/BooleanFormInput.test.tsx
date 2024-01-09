642b92cb3fd6a0e8c5f33022aad2a7756377e32d
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2024-01-09 21:35:11 +0800

    Fix optional boolean input -iso6

--- src/UI/Components/ServiceInstanceForm/Components/BooleanFormInput.test.tsx
+++ src/UI/Components/ServiceInstanceForm/Components/BooleanFormInput.test.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import { BooleanFormInput } from "./BooleanFormInput";
 
 describe("BooleanFormInput", () => {
@@ -31,5 +33,51 @@ describe("BooleanFormInput", () => {
     );
     const radioButtons = await screen.findAllByRole("radio");
     expect(radioButtons.length).toEqual(3);
+  });
+
+  it("Should render radio button input for optional boolean parameters", async () => {
+    const mockedFn = jest.fn();
+    render(
+      <BooleanFormInput
+        attributeName="opt_bool_param"
+        isChecked={true}
+        isOptional={true}
+        description={"This is a bool parameter"}
+        handleInputChange={(value) => mockedFn(value)}
+      />,
+    );
+
+    await act(async () => {
+      await userEvent.click(await screen.findByTestId("opt_bool_param-false"));
+    });
+    expect(mockedFn).toHaveBeenCalledWith(false);
+
+    await act(async () => {
+      await userEvent.click(await screen.findByTestId("opt_bool_param-none"));
+    });
+    expect(mockedFn).toHaveBeenCalledWith("null");
+  });
+
+  it("Should render radio button input for optional boolean parameters", async () => {
+    const mockedFn = jest.fn();
+    render(
+      <BooleanFormInput
+        attributeName="opt_bool_param"
+        isChecked={false}
+        isOptional={true}
+        description={"This is a bool parameter"}
+        handleInputChange={(value) => mockedFn(value)}
+      />,
+    );
+
+    await act(async () => {
+      await userEvent.click(await screen.findByTestId("opt_bool_param-true"));
+    });
+    expect(mockedFn).toHaveBeenCalledWith(true);
+
+    await act(async () => {
+      await userEvent.click(await screen.findByTestId("opt_bool_param-none"));
+    });
+    expect(mockedFn).toHaveBeenCalledWith("null");
   });
 });
