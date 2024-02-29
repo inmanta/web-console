2b3cc50ee9f2c6ca41786eba791fcdffe325505e
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2024-02-29 15:51:40 +0800

    docstrings

--- src/UI/Components/Diagram/helpers.ts
+++ src/UI/Components/Diagram/helpers.ts
@@ -442,13 +442,22 @@ export const findCorrespondingId = (
   })).find(({ id }) => id === instanceAsTable.id);
 };
 
+/**
+ * Updates the position of a label relative to a link's target or source side.
+ * @param {"target" | "source"} side - The side of the link where the label is positioned. Can be "target" or "source".
+ * @param {g.Rect} _refBBox - The bounding box of a reference element (unused).
+ * @param {SVGSVGElement} node - The SVG element representing the label.
+ * @param {{ [key: string]: unknown }} _attrs - Additional attributes for the label (unused).
+ * @param {LabelLinkView} linkView - The view representing the link associated with the label.
+ * @returns {{ textAnchor: "start" | "end", x: number, y: number }} - An object containing the updated attributes for the label.
+ */
 export const updateLabelPosition = (
   side: "target" | "source",
   _refBBox: g.Rect,
   node: SVGSVGElement,
   _attrs: { [key: string]: unknown },
   linkView: LabelLinkView, //dia.LinkView & dia.Link doesn't have sourceView or targetView properties in the model
-) => {
+): { textAnchor: "start" | "end"; x: number; y: number } => {
   let textAnchor, tx, ty, viewCoordinates, anchorCoordinates;
   if (side === "target") {
     viewCoordinates = linkView.targetView.model.position();
