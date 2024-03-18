303a73f03b9682046949added70471a9ac448ed4
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2024-03-18 21:02:56 +0800

    fix

--- src/Data/API/BaseApiHelper.test.ts
+++ src/Data/API/BaseApiHelper.test.ts
@@ -17,6 +17,24 @@ test("BaseApiHelper.get executes a GET request with correct url & env", async ()
   expect(requestInit?.headers).toEqual({ "X-Inmanta-Tid": env });
 });
 
+test("BaseApiHelper.post executes a POST request with correct url & env", async () => {
+  const apiHelper = new BaseApiHelper();
+  const url = "/test-url";
+  const env = "environment_a";
+
+  fetchMock.mockResponse(JSON.stringify({ data: [] }));
+  expect(await apiHelper.post(url, env, {})).toEqual(
+    Either.right({ data: [] }),
+  );
+
+  const [receivedUrl, requestInit] = fetchMock.mock.calls[0];
+  expect(receivedUrl).toEqual(url);
+  expect(requestInit?.headers).toEqual({
+    "Content-Type": "application/json",
+    "X-Inmanta-Tid": env,
+  });
+});
+
 test("BaseApiHelper.get handles a failed a GET request", async () => {
   const apiHelper = new BaseApiHelper();
   const url = "/test-url";
