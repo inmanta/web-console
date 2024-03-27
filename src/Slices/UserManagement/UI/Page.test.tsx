bbd009fabad8724948a0b92a6699d6e3f9dd2005
laura stordeur <stordeur.laura@outlook.fr>
2024-03-27 18:54:23 +0100

    Add correct act import

--- src/Slices/UserManagement/UI/Page.test.tsx
+++ src/Slices/UserManagement/UI/Page.test.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { HttpResponse, http } from "msw";
 import { setupServer } from "msw/node";
-import { act } from "react-dom/test-utils";
 import { PrimaryAuthController } from "@/Data";
 import { UserInfo } from "@/Data/Managers/V2/GetUsers";
 import { dependencies } from "@/Test";
