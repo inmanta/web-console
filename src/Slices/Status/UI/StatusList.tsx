b236ec2efa1eb14e7f1674a4e65c0dd3d680b9f9
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2024-11-06 17:25:08 +0100

    Support nested status properties in the Status Page (Issue #6018, PR #6023)
    # Description
    
    Fix the issue that nested properties in status list crash the Page.
    
    ![Screenshot 2024-11-06 at 12 06 15](https://github.com/user-attachments/assets/3b5af4ca-92b5-4619-815c-22578e7d7496)
    
    closes #6018
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] Attached issue to pull request
    - [ ] Changelog entry
    - [ ] Code is clear and sufficiently documented
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )

--- src/Slices/Status/UI/StatusList.tsx
+++ src/Slices/Status/UI/StatusList.tsx
@@ -18,6 +18,15 @@ interface Props {
   className?: string;
 }
 
+/**
+ * Renders a list of status items, including product status, API status, web console status, extensions, and slices.
+ *
+ * @props {Props} props - The properties for the status list component.
+ * @prop {ServerStatus} status - The server status object
+ * @prop {string} apiUrl - The API URL to display in the status list.
+ * @prop {string} [className] - Optional additional class name for the list.
+ * @returns {React.FC<Props>} The rendered status list component.
+ */
 export const StatusList: React.FC<Props> = ({
   status,
   apiUrl,
@@ -96,5 +105,52 @@ export const StatusList: React.FC<Props> = ({
   );
 };
 
-const toDetails = (obj: Record<string, unknown>): [string, string][] =>
-  Object.entries(obj).map(([key, value]) => [key, `${value}`]);
+type DetailKey = string;
+
+type DetailValue = Record<string, string> | string;
+export type DetailTuple = [DetailKey, DetailValue];
+
+/**
+ * Converts a Record<string, unknown> to an array of key-value pairs where values are either strings or nested records with string values.
+ *
+ * This function iterates over the entries of the provided record and converts each value to a string. If a value is a nested record, it recursively converts all nested values to strings.
+ * We know from the core team that we can safely assume that we don't need to handle nested records more than one level deep.
+ *
+ * @param {Record<string, unknown>} obj - The record to convert.
+ * @returns {DetailTuple[]} An array of key-value pairs where values are either strings or nested records with string values.
+ */
+const toDetails = (obj: Record<string, unknown>): DetailTuple[] =>
+  Object.entries(obj).map(([key, value]) => {
+    return [
+      key,
+      isRecord(value) ? stringifyRecordAttributes(value) : `${value}`,
+    ];
+  });
+
+/**
+ * Converts all attributes of a Record<string, unknown> to strings.
+ *
+ * This function iterates over the entries of the provided record and converts each value to a string.
+ *
+ * @param {Record<string, unknown>} obj - The record whose attributes are to be converted to strings.
+ * @returns {Record<string, string>} A new record with all values converted to strings.
+ */
+const stringifyRecordAttributes = (
+  obj: Record<string, unknown>,
+): Record<string, string> => {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, String(value)]),
+  );
+};
+
+/**
+ * Type guard to check if a value is a Record<string, unknown>.
+ *
+ * This function checks if the provided value is an object, is not null, and is not an array.
+ *
+ * @param {unknown} value - The value to check.
+ * @returns {value is Record<string, unknown>} True if the value is a Record<string, unknown>, otherwise false.
+ */
+const isRecord = (value: unknown): value is Record<string, unknown> => {
+  return typeof value === "object" && value !== null && !Array.isArray(value);
+};
