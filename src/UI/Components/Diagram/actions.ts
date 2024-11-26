a3c39e4b1195544cfba24157edf6129286dc94e7
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2024-11-26 11:24:35 +0100

    change order

--- src/UI/Components/Diagram/actions.ts
+++ src/UI/Components/Diagram/actions.ts
@@ -164,7 +164,7 @@ export function appendInstance(
       isMainInstance && !serviceInstanceModel.strict_modifier_enforcement,
     isBlockedFromEditing:
       !serviceInstanceModel.strict_modifier_enforcement || isBlockedFromEditing,
-    stencilName: isMainInstance ? undefined : stencilName,
+    stencilName: !isMainInstance ? stencilName : undefined,
     id: instanceWithRelations.instance.id,
   });
 
