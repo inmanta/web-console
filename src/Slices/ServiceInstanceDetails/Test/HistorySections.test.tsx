10359eb4c0ea20ea4433bd78b70613eec25089cd
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-01-15 15:56:28 +0100

    test

---
+++ src/Slices/ServiceInstanceDetails/Test/HistorySections.test.tsx
@@ -0,0 +1,91 @@
+import { ServiceModel } from "@/Core";
+import { InstanceDetailsContext } from "../Core/Context";
+import { HistorySection } from "../UI/Components/Sections";
+import { historyData, instanceData } from "./mockData";
+import { UseInfiniteQueryResult, UseQueryResult } from "@tanstack/react-query";
+import { InstanceLog } from "@/Core/Domain/HistoryLog";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { SetupWrapper } from "./mockSetup";
+import exp from "constants";
+import userEvent from "@testing-library/user-event";
+
+const setup = (hasNextPage: boolean, hasPreviousPage: boolean) => {
+  const fetchNextPage = jest.fn();
+  const fetchPreviousPage = jest.fn();
+
+  const component = (
+    <SetupWrapper expertMode={false}>
+      <InstanceDetailsContext.Provider
+        value={{
+          instance: instanceData,
+          logsQuery: {
+            data: historyData,
+            hasNextPage,
+            hasPreviousPage,
+            fetchNextPage,
+            fetchPreviousPage,
+            isLoading: false,
+            isError: false,
+            isSuccess: true,
+          } as unknown as UseInfiniteQueryResult<InstanceLog[], Error>,
+          serviceModelQuery: {} as UseQueryResult<ServiceModel, Error>,
+        }}
+      >
+        <HistorySection />
+      </InstanceDetailsContext.Provider>
+    </SetupWrapper>
+  );
+
+  return {
+    component,
+    fetchNextPage,
+    fetchPreviousPage,
+  };
+};
+
+//I test that the HistorySection infinite query render functionalities as the component as a whole is being testes in the PAge.test.tsx file
+describe("HistorySection infinite query", () => {
+  it("should render", () => {
+    const { component } = setup(false, false);
+
+    render(component);
+
+    expect(
+      screen.getByRole("grid", { name: "VersionHistoryTable" }),
+    ).toBeInTheDocument();
+
+    const buttonPrevious = screen.queryByRole("button", {
+      name: "Load Previous",
+    });
+    const buttonNewer = screen.queryByRole("button", { name: "Load Newer" });
+
+    expect(buttonPrevious).toBeNull();
+    expect(buttonNewer).toBeNull();
+  });
+
+  it("should render with functional button to fetch newer versions", async () => {
+    const { component, fetchPreviousPage } = setup(false, true);
+
+    render(component);
+
+    const button = screen.getByRole("button", { name: "Load Newer" });
+    expect(button).toBeInTheDocument();
+
+    await userEvent.click(button);
+
+    expect(fetchPreviousPage).toHaveBeenCalled();
+  });
+
+  it("should render with functional button to fetch previous versions", async () => {
+    const { component, fetchNextPage } = setup(true, false);
+
+    render(component);
+
+    const button = screen.getByRole("button", { name: "Load Previous" });
+    expect(button).toBeInTheDocument();
+
+    await userEvent.click(button);
+    expect(fetchNextPage).toHaveBeenCalled();
+  });
+});
