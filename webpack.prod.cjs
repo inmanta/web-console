923c32dc1a1d7ef1712e79cb175bd639240f9d22
Lukas <stordeur.laura@outlook.fr>
2025-04-02 11:44:24 +0200

    fix prod build

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -54,9 +54,15 @@ module.exports = merge(common, {
       {
         test: /\.css$/,
         include: [
+          path.resolve(__dirname, "node_modules/monaco-editor"),
+        ],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.css$/,
+        include: [
           path.resolve(__dirname, "src"),
           path.resolve(__dirname, "node_modules/patternfly"),
-          path.resolve(__dirname, "node_modules/monaco-editor"),
           path.resolve(__dirname, "node_modules/@patternfly/patternfly"),
           path.resolve(__dirname, "node_modules/@patternfly/react-styles/css"),
           path.resolve(
