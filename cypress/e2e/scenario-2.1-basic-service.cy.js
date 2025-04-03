f86c850ccf965d352c6e216d3ac69c6d0ec797f8
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-04-03 16:02:08 +0200

    Fix bug with visibility of nested fields in input where they are set to null (Issue #6252, PR #6253)
    Pull request opened by the merge tool on behalf of #6253

--- cypress/e2e/scenario-2.1-basic-service.cy.js
+++ cypress/e2e/scenario-2.1-basic-service.cy.js
@@ -149,7 +149,7 @@ if (Cypress.env("edition") === "iso") {
       cy.get('[aria-label="InstanceRow-Intro"]').should("have.length", 1);
 
       // check whether there are two options available in the dropdown to copy the id/identifier.
-      cy.get('[aria-label="IdentityCell-basic-service"]').click();
+      cy.get('[aria-label="Copy to clipboard"]').eq(0).click();
       cy.get('[role="menuitem"]').should("have.length", 2);
     });
 
