2f965f29e0c6cdf4fb3b3725ab74aee7a0c51d92
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-04-08 17:40:00 +0200

    lint

--- src/Slices/Resource/UI/ResourcesPage/Page.tsx
+++ src/Slices/Resource/UI/ResourcesPage/Page.tsx
@@ -1,11 +1,7 @@
 import React, { useEffect } from "react";
 import { Flex, FlexItem } from "@patternfly/react-core";
 import { Resource } from "@/Core";
-import {
-  useUrlStateWithFilter,
-  useUrlStateWithPageSize,
-  useUrlStateWithSort,
-} from "@/Data";
+import { useUrlStateWithFilter, useUrlStateWithPageSize, useUrlStateWithSort } from "@/Data";
 import { useUrlStateWithCurrentPage } from "@/Data/Common/UrlState/useUrlStateWithCurrentPage";
 import { useGetResources } from "@/Data/Managers/V2/Resource/GetResources/useGetResources";
 import {
@@ -21,9 +17,7 @@ import { ResourcesTableProvider } from "./ResourcesTableProvider";
 import { Summary } from "./Summary";
 
 const Wrapper: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
-  <PageContainer pageTitle={words("inventory.tabs.resources")}>
-    {children}
-  </PageContainer>
+  <PageContainer pageTitle={words("inventory.tabs.resources")}>{children}</PageContainer>
 );
 
 export const Page: React.FC = () => {
@@ -33,20 +27,17 @@ export const Page: React.FC = () => {
   const [pageSize, setPageSize] = useUrlStateWithPageSize({
     route: "Resources",
   });
-  const [filter, setFilter] =
-    useUrlStateWithFilter<Resource.FilterWithDefaultHandling>({
-      route: "Resources",
-      keys: { disregardDefault: "Boolean" },
-    });
+  const [filter, setFilter] = useUrlStateWithFilter<Resource.FilterWithDefaultHandling>({
+    route: "Resources",
+    keys: { disregardDefault: "Boolean" },
+  });
   const [sort, setSort] = useUrlStateWithSort<Resource.SortKey>({
     default: { name: "resource_type", order: "asc" },
     route: "Resources",
   });
 
   const filterWithDefaults =
-    !filter.disregardDefault && !filter.status
-      ? { ...filter, status: ["!orphaned"] }
-      : filter;
+    !filter.disregardDefault && !filter.status ? { ...filter, status: ["!orphaned"] } : filter;
 
   const { data, isSuccess, isError, refetch, error } = useGetResources({
     pageSize,
@@ -61,18 +52,11 @@ export const Page: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sort.order]);
 
-  const updateFilter = (
-    updater: (filter: Resource.Filter) => Resource.Filter,
-  ): void => setFilter(updater(filterWithDefaults));
+  const updateFilter = (updater: (filter: Resource.Filter) => Resource.Filter): void =>
+    setFilter(updater(filterWithDefaults));
 
   if (isError) {
-    return (
-      <ErrorView
-        message={error.message}
-        ariaLabel="ResourcesPage-Error"
-        retry={refetch}
-      />
-    );
+    return <ErrorView message={error.message} ariaLabel="ResourcesPage-Error" retry={refetch} />;
   }
 
   if (isSuccess) {
@@ -92,10 +76,7 @@ export const Page: React.FC = () => {
           setFilter={setFilter}
         />
         {data.data.length <= 0 ? (
-          <EmptyView
-            message={words("resources.empty.message")}
-            aria-label="ResourcesPage-Empty"
-          />
+          <EmptyView message={words("resources.empty.message")} aria-label="ResourcesPage-Empty" />
         ) : (
           <>
             <ResourcesTableProvider
