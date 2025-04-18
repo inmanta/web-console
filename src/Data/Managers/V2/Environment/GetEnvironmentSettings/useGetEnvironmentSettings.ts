a5b24dc53af03a94748a43bea8b751b3edd17e94
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-04-18 17:10:07 +0200

    Env Queries

---
+++ src/Data/Managers/V2/Environment/GetEnvironmentSettings/useGetEnvironmentSettings.ts
@@ -0,0 +1,30 @@
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { EnvironmentSettings } from "@/Core/Domain/EnvironmentSettings";
+import { useGetWithoutEnv } from "../../helpers";
+
+/**
+ * Return Signature of the useGetEnvironmentSettings React Query
+ */
+interface GetEnvironmentSettings {
+  useOneTime: (envId: string) => UseQueryResult<EnvironmentSettings, Error>;
+}
+
+/**
+ * React Query hook for fetching environment settings.
+ *
+ * @returns {GetEnvironmentSettings} An object containing the different available queries.
+ * @returns {UseQueryResult<EnvironmentSettings, Error>} returns.useOneTime - Fetch environment settings with a single query.
+ */
+export const useGetEnvironmentSettings = (): GetEnvironmentSettings => {
+  const get = useGetWithoutEnv()<{ data: EnvironmentSettings }>;
+
+  return {
+    useOneTime: (envId: string): UseQueryResult<EnvironmentSettings, Error> =>
+      useQuery({
+        queryKey: ["get_environment_settings-one_time", envId],
+        queryFn: () => get(`/api/v2/environment_settings/${envId}`),
+        retry: false,
+        select: (data) => data.data,
+      }),
+  };
+};
