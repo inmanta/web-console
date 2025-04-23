cdfe776c48d9ec2157bdd8fd32e6d0530c6f1111
Lukas <stordeur.laura@outlook.fr>
2025-04-23 13:31:57 +0200

    feedback

--- src/UI/Routing/CustomRouter.tsx
+++ src/UI/Routing/CustomRouter.tsx
@@ -2,10 +2,26 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 /**
- * CustomRouter is base for implementation usePrompt hook which allows to halt and resume navigation on demand.
+ * CustomRouter is a wrapper component that provides the necessary router context for the usePrompt hook to function.
+ * The usePrompt hook (from react-router) requires a router context to work properly, which this component provides.
  *
- * As for version 6.4 of react-router that package doesn't share logic mentioned above which we wanted to use in Form compoents and
- * their (stable) implementations of routers doesn't accept pre-initiated history object as parameter, which is essential to make this possible
+ * This component is essential for implementing navigation blocking functionality, allowing you to:
+ * - Prompt users before they leave a page
+ * - Prevent accidental navigation when there are unsaved changes
+ * - Handle browser refresh/close events
+ *
+ * @component
+ * @param {Object} props - The component props
+ * @param {string} [props.basename] - The base URL for all locations. If your app is served from a sub-directory on your server, you'll want to set this to the sub-directory.
+ * @param {React.ReactNode} props.children - The child components to be rendered within the router
+ * @returns {JSX.Element} A RouterProvider component configured with the specified basename and children
+ *
+ * @example
+ * ```tsx
+ * <CustomRouter basename="/app">
+ *   <App />
+ * </CustomRouter>
+ * ```
  */
 function CustomRouter({ basename, children }: { basename?: string; children: React.ReactNode }) {
   const router = createBrowserRouter(
