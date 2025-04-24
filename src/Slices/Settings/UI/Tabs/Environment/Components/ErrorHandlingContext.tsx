2fd4285da129e27dc30887f312ca12bb4b0971f3
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-04-24 13:58:36 +0200

    docstrings

--- src/Slices/Settings/UI/Tabs/Environment/Components/ErrorHandlingContext.tsx
+++ src/Slices/Settings/UI/Tabs/Environment/Components/ErrorHandlingContext.tsx
@@ -2,6 +2,9 @@ import React, { createContext, useState } from "react";
 
 /**
  * ErrorHandlingContext component for the EnvironmentSettings component
+ * After updating communication to react query flow of the inline edit inputs changed and to avoid prop drilling
+ * this context was created to handle the error state
+ *
  * @returns {React.FC<React.PropsWithChildren<unknown>>} The ErrorHandlingContext component
  */
 export const ErrorHandlingContext = createContext<{
@@ -14,6 +17,9 @@ export const ErrorHandlingContext = createContext<{
 
 /**
  * ErrorHandlingProvider component for the EnvironmentSettings component
+ *
+ * Provider for the ErrorHandlingContext, it provides the error state and the function to set the error state
+ *
  * @returns {React.FC<React.PropsWithChildren<unknown>>} The ErrorHandlingProvider component
  */
 export const ErrorHandlingProvider: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => {
