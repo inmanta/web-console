ac0ade9aa09cf6dc8ffbcda4006badf669f88a97
Lukas <stordeur.laura@outlook.fr>
2025-04-25 13:26:06 +0200

    Replace old Login page implementation with Patternfly Login page. Repair redirects to login page and cover trailing slashes in routes. (Issue #6131, PR #6316)
    # Description
    
    - Repair the redirection issues, and cover trailing slashes to prevent not found pages.
    - Updated the Login page to use the Patternfly component instead; This means we lose the "show/hide" functionality on the password field.
    - Updated the button in the userManagement modal to not use the Content element inside the button, as Content elements by default have black text, and that the button doesn't plain text to be wrapped.
    
    closes #6131
    
    https://github.com/user-attachments/assets/639dc5b6-c02d-4680-b5ea-a7c656eedf0f
    
    ![image](https://github.com/user-attachments/assets/4b281ac5-4dd1-409c-8fa2-2db1c0909667)
    
    ![image](https://github.com/user-attachments/assets/78bd991a-c8df-42f1-9be4-76151dd0b4de)
    
    ![image](https://github.com/user-attachments/assets/64c9bdce-f37b-438b-8ce4-45136f174009)

--- src/UI/Routing/PrimaryBaseUrlManager.ts
+++ src/UI/Routing/PrimaryBaseUrlManager.ts
@@ -9,14 +9,14 @@ export class PrimaryBaseUrlManager implements BaseUrlManager {
   ) {}
 
   getBasePathname(): string {
-    const { pathname, anchor } = this;
+    const { anchor } = this;
+    // Normalize the pathname by removing trailing slash
+    const normalizedPathname = this.pathname.replace(/\/$/, "");
 
-    if (!pathname.includes(anchor)) return anchor;
+    if (!normalizedPathname.includes(anchor)) return anchor;
+    if (normalizedPathname.split(anchor).length > 2) return anchor;
 
-    if (pathname.split(anchor).length > 2) return anchor;
-
-    const [pre] = pathname.split(anchor);
-
+    const [pre] = normalizedPathname.split(anchor);
     return `${pre}${anchor}`;
   }
 
