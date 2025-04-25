f4a60b99c0bd0fea299140f81e00e7c9ab868e55
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-04-25 15:23:43 +0200

    remarks

--- src/Test/Data/Environment.ts
+++ src/Test/Data/Environment.ts
@@ -8,7 +8,27 @@ export const env: FlatEnvironment = {
   repo_url: "",
   halted: false,
   projectName: "project_name_env",
-  settings: {},
+  settings: {
+    auto_deploy: false,
+    server_compile: true,
+    autostart_splay: 10,
+    purge_on_delete: true,
+    autostart_on_start: true,
+    autostart_agent_map: {
+      internal: "local:",
+    },
+    enable_lsm_expert_mode: false,
+    push_on_auto_deploy: true,
+    protected_environment: false,
+    autostart_agent_interval: 600,
+    resource_action_logs_retention: 7,
+    autostart_agent_deploy_interval: 600,
+    autostart_agent_repair_interval: 86400,
+    environment_agent_trigger_method: "push_full_deploy",
+    autostart_agent_deploy_splay_time: 10,
+    autostart_agent_repair_splay_time: 600,
+    agent_trigger_method_on_auto_deploy: "push_incremental_deploy",
+  },
 };
 
 export const a: EnvironmentModel = {
@@ -58,27 +78,7 @@ export const e: EnvironmentModel = {
   repo_branch: "",
   repo_url: "",
   halted: false,
-  settings: {
-    auto_deploy: false,
-    server_compile: true,
-    autostart_splay: 10,
-    purge_on_delete: true,
-    autostart_on_start: true,
-    autostart_agent_map: {
-      internal: "local:",
-    },
-    enable_lsm_expert_mode: false,
-    push_on_auto_deploy: true,
-    protected_environment: false,
-    autostart_agent_interval: 600,
-    resource_action_logs_retention: 7,
-    autostart_agent_deploy_interval: 600,
-    autostart_agent_repair_interval: 86400,
-    environment_agent_trigger_method: "push_full_deploy",
-    autostart_agent_deploy_splay_time: 10,
-    autostart_agent_repair_splay_time: 600,
-    agent_trigger_method_on_auto_deploy: "push_incremental_deploy",
-  },
+  settings: {},
 };
 
 export const filterable: EnvironmentExpertOnly[] = [
