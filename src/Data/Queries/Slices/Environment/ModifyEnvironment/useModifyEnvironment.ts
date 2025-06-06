2640dd3fa77f5eebd5f5807e95caf49df90cfdab
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-06-06 15:37:36 +0200

    e2e  fiix

--- src/Data/Queries/Slices/Environment/ModifyEnvironment/useModifyEnvironment.ts
+++ src/Data/Queries/Slices/Environment/ModifyEnvironment/useModifyEnvironment.ts
@@ -6,7 +6,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import { ModifyEnvironmentParams } from "@/Core";
-import { usePost } from "@/Data/Queries";
+import { getPartialEnvironmentsKey, usePost } from "@/Data/Queries";
 import { DependencyContext } from "@/UI";
 import { getEnvironmentDetailsKey } from "../GetEnvironmentDetails";
 import { getEnvironmentsKey } from "../GetEnvironments";
@@ -31,6 +31,7 @@ export const useModifyEnvironment = (
     onSuccess: () => {
       client.refetchQueries({ queryKey: getEnvironmentDetailsKey.root() });
       client.refetchQueries({ queryKey: getEnvironmentsKey.root() });
+      client.refetchQueries({ queryKey: getPartialEnvironmentsKey.root() });
     },
     ...options,
   });
