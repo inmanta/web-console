0e6de860f1cee6704221d85e089f6c7f059d9b6b
Mateusz Borowczyk <mateusz.borowczyk@inmanta.com>
2025-06-20 13:06:01 +0200

    workaround

--- src/Data/Queries/Slices/Auth/AddRoleToUser/useAddRoleToUser.ts
+++ src/Data/Queries/Slices/Auth/AddRoleToUser/useAddRoleToUser.ts
@@ -2,6 +2,8 @@ import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { usePostWithoutEnv } from "@/Data/Queries";
 import { UserRole } from "../GetUserRoles/useGetUserRoles";
 
+
+
 /**
  * Custom hook for adding a role to a user.
  * @param {string} user - The username of the user to add the role to.
@@ -14,10 +16,17 @@ export const useAddRoleToUser = ({
   user: string;
   options?: UseMutationOptions<void, Error, UserRole>;
 }) => {
-  const post = usePostWithoutEnv()<UserRole>;
+  //until core won't align endpoints we need to do conversion at this point
+  const post = usePostWithoutEnv()<{
+    role: string;
+    environment: string;
+  }>;
 
   return useMutation<void, Error, UserRole>({
-    mutationFn: (body) => post(`/api/v2/role_assignment/${user}`, body),
+    mutationFn: (body) => post(`/api/v2/role_assignment/${user}`, {
+      role: body.name,
+      environment: body.environment,
+    }),
     mutationKey: ["add_role_to_user", user],
     ...options,
   });
