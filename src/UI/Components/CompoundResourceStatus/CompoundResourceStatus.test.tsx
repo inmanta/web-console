4130780cb92ef7bc7bc2a428ab97bfef62f97e25
AronH99 <aron.heremans@inmanta.com>
2026-03-31 15:24:29 +0200

    fix: small fix

--- src/UI/Components/CompoundResourceStatus/CompoundResourceStatus.test.tsx
+++ src/UI/Components/CompoundResourceStatus/CompoundResourceStatus.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { mockCompoundResourceData } from "@/Test/Data/Resource";
 import { CompoundResourceStatus } from "./CompoundResourceStatus";
-import { mockCompoundResourceData } from "./mockData";
 
 const defaultProps = {
   ...mockCompoundResourceData,
