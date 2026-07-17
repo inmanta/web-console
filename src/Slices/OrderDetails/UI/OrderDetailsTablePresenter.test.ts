a930ea80b8a62d625b114571cca58db3f55a7fd2
Lukas <stordeur.lukas@outlook.fr>
2026-07-17 16:25:35 +0200

    - Default the create-token form in the Settings page to a revocable token   - Don't allow the user to make a non-revocable token in the web-console. - Default the create-token form in the Settings page to a 7-day expiry - Use proper date formatting, consitent with the rest of the web-console for the token table. - Use service_identity_attribute_value for the service instance in the order details rows.  (PR #7146)
    # Description
    
      - Default the create-token form in the Settings page to a revocable token
        - Don't allow the user to make a non-revocable token in the web-console.
      - Default the create-token form in the Settings page to a 7-day expiry
      - Use proper date formatting, consitent with the rest of the web-console for the token table.
        - revoked at, and expires at, use full timestamp
        - last used, and issued at use the relative date presentations with the full timestamp in the tooltip.
      - Use service_identity_attribute_value for the service instance in the order details rows.
      - Table header now only displays instance instead of instance id.
    
    <img width="1387" height="843" alt="image" src="https://github.com/user-attachments/assets/83bcde43-06cc-44c8-84c1-6ba6551a28b1" />
    
    <img width="1394" height="821" alt="image" src="https://github.com/user-attachments/assets/5006c193-ac5b-4b87-b961-190ef4d132da" />

--- src/Slices/OrderDetails/UI/OrderDetailsTablePresenter.test.ts
+++ src/Slices/OrderDetails/UI/OrderDetailsTablePresenter.test.ts
@@ -10,14 +10,14 @@ test("Rows should have two items", () => {
 });
 
 test("TablePresenter converts column index to name correctly", () => {
-  expect(presenter.getColumnNameForIndex(0)).toEqual("instance_id");
+  expect(presenter.getColumnNameForIndex(0)).toEqual("instance");
   expect(presenter.getColumnNameForIndex(1)).toEqual("service_entity");
   expect(presenter.getColumnNameForIndex(-1)).toBeUndefined();
   expect(presenter.getColumnNameForIndex(10)).toBeUndefined();
 });
 
 test("TablePresenter converts column name to index correctly", () => {
-  expect(presenter.getIndexForColumnName("instance_id")).toEqual(0);
+  expect(presenter.getIndexForColumnName("instance")).toEqual(0);
   expect(presenter.getIndexForColumnName("service_entity")).toEqual(1);
   expect(presenter.getIndexForColumnName(undefined)).toEqual(-1);
 });
