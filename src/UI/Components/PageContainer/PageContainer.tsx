7a714d46ebd4a35c14b48af63cb12f7e1f21040c
Lukas <stordeur.lukas@outlook.fr>
2026-08-14 11:51:31 +0200

    Agents bulk actions

--- src/UI/Components/PageContainer/PageContainer.tsx
+++ src/UI/Components/PageContainer/PageContainer.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-import { Content, PageSection, PageSectionProps } from "@patternfly/react-core";
+import { Content, Flex, FlexItem, PageSection, PageSectionProps } from "@patternfly/react-core";
 
 interface Props extends PageSectionProps {
   pageTitle: string | React.ReactNode;
+  actions?: React.ReactNode;
 }
 
 export const PageContainer: React.FC<React.PropsWithChildren<Props>> = ({
   children,
   pageTitle,
+  actions,
   ...props
 }) => (
   <>
     <PageSection hasBodyWrapper={false}>
       <Content>
-        <Content component="h1">{pageTitle}</Content>
+        {actions ? (
+          <Flex
+            alignItems={{ default: "alignItemsCenter" }}
+            justifyContent={{ default: "justifyContentSpaceBetween" }}
+          >
+            <FlexItem>
+              <Content component="h1">{pageTitle}</Content>
+            </FlexItem>
+            <FlexItem>{actions}</FlexItem>
+          </Flex>
+        ) : (
+          <Content component="h1">{pageTitle}</Content>
+        )}
       </Content>
     </PageSection>
     <PageSection hasBodyWrapper={false} {...props} isFilled padding={{ default: "padding" }}>
