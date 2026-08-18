43abf0610fc83e54cea3a0906b7c89a9a0343790
Lukas <stordeur.lukas@outlook.fr>
2026-08-18 10:25:36 +0200

    format

--- src/Data/Queries/Slices/Agents/AgentsAction/useAgentsAction.ts
+++ src/Data/Queries/Slices/Agents/AgentsAction/useAgentsAction.ts
@@ -13,7 +13,11 @@ import { getAgentKey } from "../GetAgents/useGetAgents";
  * The bulk actions that can be applied to all agents at once.
  */
 export type AgentsBulkAction =
-  "pause" | "unpause" | "keep_paused_on_resume" | "unpause_on_resume" | "remove_all_agent_venvs";
+  | "pause"
+  | "unpause"
+  | "keep_paused_on_resume"
+  | "unpause_on_resume"
+  | "remove_all_agent_venvs";
 
 /**
  * Interface for the bulk agents action parameters
