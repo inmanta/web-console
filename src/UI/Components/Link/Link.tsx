f0fef9e6d3001b2e4d1223e3d99c863f47756ec3
AronH99 <aron.heremans@inmanta.com>
2026-09-21 13:09:28 +0200

    Link each dry run and version compare diff entry to the corresponding resource details page. (Issue #6052, PR #7346)
    # Description
    
    - This PR effectively makes it possible to navigate from the diff entries of a desired state compare or a compliance check compare to a resource detail page of that entry.
    This is something Guillaume requested already a long time ago and is quite a small change for maximal benefit :)
    
    closes https://github.com/inmanta/web-console/issues/6052
    
    https://github.com/user-attachments/assets/8d8c9941-38f5-44d1-95ac-ca9392ac15ba

--- src/UI/Components/Link/Link.tsx
+++ src/UI/Components/Link/Link.tsx
@@ -11,11 +11,21 @@ interface Props {
   className?: string;
   children?: React.ReactNode;
   variant?: "plain" | "default";
+  fitContent?: boolean;
 }
 
 export const Link: React.FC<Props> = forwardRef<HTMLAnchorElement, Props>(
   (
-    { children, isDisabled, pathname, envOnly, search: newSearch, className, variant = "default" },
+    {
+      children,
+      isDisabled,
+      pathname,
+      envOnly,
+      search: newSearch,
+      className,
+      variant = "default",
+      fitContent = false,
+    },
     ref
   ) => {
     const { search: currentSearch } = useLocation();
@@ -28,16 +38,22 @@ export const Link: React.FC<Props> = forwardRef<HTMLAnchorElement, Props>(
     return isDisabled ? (
       <>{children}</>
     ) : (
-      <StyledRRLink to={{ pathname, search }} className={className} ref={ref} $variant={variant}>
+      <StyledRRLink
+        to={{ pathname, search }}
+        className={className}
+        ref={ref}
+        $variant={variant}
+        $fitContent={fitContent}
+      >
         {children}
       </StyledRRLink>
     );
   }
 );
 
-const StyledRRLink = styled(RRLink)<{ $variant?: "plain" | "default" }>`
+const StyledRRLink = styled(RRLink)<{ $variant?: "plain" | "default"; $fitContent?: boolean }>`
   display: inline-block;
-  width: 100%;
+  ${({ $fitContent }) => (!$fitContent ? "width: 100%;" : "")}
   color: ${({ $variant }) =>
     $variant === "plain" ? "inherit" : "var(--pf-t--global--text--color--link--default)"};
   ${({ $variant }) => ($variant === "plain" ? "text-decoration:none" : "")};
